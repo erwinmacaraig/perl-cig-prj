dcd05ebd179b03dca85fa1a036dc3584d65e52f2
facascante <f.cascante@sportingpulseinternational.com>
2014-09-04 23:59:35 +0000

    add, edit and delete identifier #FC-140

--- web/EntityIdentifier.pm
+++ web/EntityIdentifier.pm
@@ -19,24 +19,257 @@ use WorkFlow;
 use RecordTypeFilter;
 use RuleMatrix;
 use Data::Dumper;
+use Authorize;
+use FieldLabels;
 
 sub handleEntityIdentifiers{ 
-    my($action, $Data, $clubID)=@_;
+    my($action, $Data)=@_;
 
     my $resultHTML='';
     my $title='';
-    if($action =~/^C_ID_LIST/) {
+    if ($action =~/^C_ID_DT/) {
+        ($resultHTML,$title)=provision($action, $Data);
+    }
+    elsif($action =~/^C_ID_LIST/) {
         #List Venues
         my $tempResultHTML = '';
-        ($tempResultHTML,$title)=listEntityIdentifier($Data,$clubID);
+        ($tempResultHTML,$title)=list($Data);
         $resultHTML .= $tempResultHTML;
     }
         
     return ($resultHTML,$title);
 }
 
-sub listEntityIdentifier{
-	my($Data,$clubID) = @_;
+sub provision{
+    my ($action, $Data)=@_;
+    my $id= param('intIdentifierId') || 0;
+    my $FieldLabels   = FieldLabels::getFieldLabels( $Data, $Defs::LEVEL_CLUB );
+    
+    my $entityID = getID($Data->{'clientValues'});
+    return '' if ($id and !entityAllowed($Data, $entityID,$Defs::LEVEL_CLUB));
+    my $option='display';
+    $option='edit' if $action eq 'C_ID_DTE';
+    $option='add' if $action eq 'C_ID_DTA';
+    $option='del' if $action eq 'C_ID_DTD';
+    $id=0 if $option eq 'add';
+    
+    
+    my $client = unescape($Data->{client});
+    if($option eq 'del'){
+      my $st=qq[
+         UPDATE tblEntityIdentifier
+           SET intStatus = 0
+          WHERE intIdentifierId=?
+          ];
+       my $query = $Data->{'db'}->prepare($st);
+       $query->execute($id);
+       $query->finish;
+       my $cgi = new CGI;
+       print $cgi->header(-location => qq[$Data->{'target'}?client=$client&amp;a=C_ID_LIST]);
+    }
+    my %tempClientValues = getClient($client);
+    
+    my $intRealmID = $Data->{'Realm'} ? $Data->{'Realm'} : 0;
+    my $field=loadDetails($Data->{'db'}, $id) || ();
+    
+    my $st=qq[
+        SELECT 
+            DISTINCT
+            intIdentifierTypeID as Value,
+            strIdentifierName as Name
+        FROM
+            tblIdentifierTypes
+        WHERE 
+            intActive = 1 AND
+            intRealmID = $intRealmID
+        ORDER BY
+            strIdentifierName
+    ];
+    my ($contacts_vals,$contacts_order)=getDBdrop_down_Ref($Data->{'db'},$st,'');
+    
+    my %FieldDefinitions = (
+      fields=>  {
+       intIdentifierTypeID => {
+          label       => $FieldLabels->{'intIdentifierTypeID'},
+          value       => $field->{intIdentifierTypeID},
+          type        => 'lookup',
+          options     => $contacts_vals,
+          sectionname => 'details',
+          firstoption => [ '', 'Select Types' ],
+        },
+        strIdentifier => {
+          label => $FieldLabels->{'strIdentifier'},
+          value => $field->{strIdentifier},
+          type  => 'text',
+          size  => '35',
+          maxsize => '250',
+          sectionname => 'details',
+        },
+        dtValidFrom=> {
+          label       => $FieldLabels->{'dtValidFrom'},
+          value       => $field->{dtValidFrom},
+          type        => 'date',
+          datetype    => 'dropdown',
+          format      => 'dd/mm/yyyy',
+          sectionname => 'details',
+          validate    => 'DATE',
+        },
+        dtValidUntil=> {
+          label       => $FieldLabels->{'dtValidUntil'},
+          value       => $field->{dtValidUntil},
+          type        => 'date',
+          datetype    => 'dropdown',
+          format      => 'dd/mm/yyyy',
+          sectionname => 'details',
+          validate    => 'DATE',
+        },
+        strDescription => {
+           label => $FieldLabels->{'strDescription'},
+           value => $field->{strDescription},
+           type => 'textarea',
+           rows => '10',
+           cols => '40',
+           sectionname => 'details',
+        },
+      },
+     order => [qw(
+        intIdentifierTypeID
+        strIdentifier
+        dtValidFrom
+        dtValidUntil
+        strDescription
+        
+      )],
+      sections => [ 
+        [ 'details', "Identifier Details" ], 
+    ],
+    options => {
+      labelsuffix => ':',
+      hideblank => 1,
+      target => $Data->{'target'},
+      formname => 'n_form',
+      submitlabel => $Data->{'lang'}->txt('Update'),
+      introtext => $Data->{'lang'}->txt('HTMLFORM_INTROTEXT'),
+      NoHTML => 1, 
+      updateSQL => qq[
+          UPDATE tblEntityIdentifier
+            SET --VAL--
+          WHERE intIdentifierId=$id
+      ],
+      addSQL => qq[
+          INSERT INTO tblEntityIdentifier (
+              intEntityID, 
+              intRealmID, 
+              --FIELDS-- 
+          )
+          VALUES (
+              $entityID,
+              $intRealmID,
+              --VAL-- 
+          )
+      ],
+      auditFunction=> \&auditLog,
+      auditAddParams => [
+        $Data,
+        'Add',
+        'Entity Identifier'
+      ],
+      auditEditParams => [
+        $id,
+        $Data,
+        'Update',
+        'Entity Identifier'
+      ],
+
+      afteraddFunction => \&postAdd,
+      afteraddParams => [$option,$Data,$Data->{'db'}],
+      afterupdateFunction => \&postUpdate,
+      afterupdateParams => [$option,$Data,$Data->{'db'}, $id],
+
+      LocaleMakeText => $Data->{'lang'},
+    },
+    carryfields =>  {
+      client => $client,
+      a=> $action,
+      intIdentifierId=> $id,
+    },
+    );
+    my $resultHTML='';
+    ($resultHTML, undef )=handleHTMLForm(\%FieldDefinitions, undef, $option, '',$Data->{'db'});
+    my $title=qq[Entity- Idetifier];
+
+    my $chgoptions='';
+    if($option eq 'display')  {
+        # Edit Venue.
+        $chgoptions.=qq[<span class = "button-small generic-button"><a href="$Data->{'target'}?client=$client&amp;a=C_ID_DTE&amp;intIdentifierId=$id">].$Data->{'lang'}->txt('Edit Venue').qq[</a></span> ];
+    }
+    elsif ($option eq 'edit') {
+        $chgoptions.=qq[<span class = "button-small generic-button"><a href="$Data->{'target'}?client=$client&amp;a=C_ID_DTD&amp;intIdentifierId=$id" onclick="return confirm('Are you sure you want to delete this Identifer');">Delete Identifier</a> ];
+    }
+    $chgoptions=qq[<div class="changeoptions">$chgoptions</div>] if $chgoptions;
+    $title=$chgoptions.$title;
+    
+    $title="Add New Identifier" if $option eq 'add';
+     my $text = qq[<p style = "clear:both;"><a href="$Data->{'target'}?client=$client&amp;a=C_ID_LIST">Click here</a> to return to list of Identifer</p>];
+    $resultHTML = $text.$resultHTML.$text;
+    return ($resultHTML,$title);
+}
+
+sub postAdd{
+
+    my($id,$params,$action,$Data,$db)=@_;
+    return undef if !$db;
+    if($action eq 'add')  {
+       my $cl=setClient($Data->{'clientValues'}) || '';
+       my %cv=getClient($cl);
+       my $clm=setClient(\%cv);
+       return (0,qq[
+         <div class="OKmsg"> Identifier Added Successfully</div><br>
+         <a href="$Data->{'target'}?client=$cl&amp;intIdentifierId=$id&amp;a=C_ID_DTE">Display Details</a><br><br>
+         <b>or</b><br><br>
+         <a href="$Data->{'target'}?client=$cl&amp;a=C_ID_DTA">Add another Identifier</a>
+
+        ]);
+    }
+}
+
+sub postUpdate {
+  my($id,$params,$action,$Data,$db, $entityID)=@_;
+  return undef if !$db;
+  $entityID ||= $id || 0;
+
+}
+
+sub loadDetails {
+  my($db, $id) = @_;
+                                                                                                        
+  my $statement=qq[
+    SELECT 
+        EI.intIdentifierId,
+        EI.intEntityID,
+        EI.intRealmID,
+        EI.intIdentifierTypeID,
+        IT.strIdentifierName,
+        EI.strIdentifier,
+        DATE_FORMAT(EI.dtValidFrom,"%d/%m/%Y") as dtValidFrom,
+        DATE_FORMAT(EI.dtValidUntil,"%d/%m/%Y") as dtValidUntil,
+        EI.strDescription
+      FROM tblEntityIdentifier AS EI 
+        INNER JOIN tblIdentifierTypes as IT ON EI.intIdentifierTypeID=IT.intIdentifierTypeID
+      WHERE IT.intActive = 1
+        AND EI.intIdentifierId = ?
+  ];
+  my $query = $db->prepare($statement);
+  $query->execute($id);
+  my $field=$query->fetchrow_hashref();
+  $query->finish;
+                                                                                                        
+  foreach my $key (keys %{$field})  { if(!defined $field->{$key}) {$field->{$key}='';} }
+  return $field;
+}
+
+sub list{
+	my($Data) = @_;
 
     my $resultHTML = '';
     my $client = unescape($Data->{client});
@@ -59,12 +292,13 @@ sub listEntityIdentifier{
       FROM tblEntityIdentifier AS EI 
         INNER JOIN tblIdentifierTypes as IT ON EI.intIdentifierTypeID=IT.intIdentifierTypeID
       WHERE IT.intActive = 1
+        AND EI.intStatus = 0
         AND EI.intEntityID = ?
       ORDER BY EI.intIdentifierId
     ];
     
     my $query = $Data->{'db'}->prepare($statement);
-    $query->execute($clubID);
+    $query->execute($entityID);
     
     my $results=0;
     my @rowdata = ();
@@ -80,7 +314,7 @@ sub listEntityIdentifier{
         dtValidFrom => $dref->{'dtValidFrom'},
         dtValidUntil => $dref->{'dtValidUntil'},
         strDescription => $dref->{'strDescription'},
-        SelectLink => "$Data->{'target'}?client=$client&amp;a=C_ID_EDIT",
+        SelectLink => "$Data->{'target'}?client=$client&amp;a=C_ID_DTE&amp;intIdentifierId=$dref->{'intIdentifierId'}",
       };
     }
     $query->finish;
@@ -88,7 +322,7 @@ sub listEntityIdentifier{
     my $title=qq[Identifiers];
     {
         my $tempClient = setClient(\%tempClientValues);
-        $addlink=qq[<span class = "button-small generic-button"><a href="$Data->{'target'}?client=$client&amp;a=C_ID_ADD">].$Data->{'lang'}->txt('Add').qq[</a></span>];
+        $addlink=qq[<span class = "button-small generic-button"><a href="$Data->{'target'}?client=$client&amp;a=C_ID_DTA">].$Data->{'lang'}->txt('Add').qq[</a></span>];
 
     }
     my $modoptions=qq[<div class="changeoptions">$addlink</div>];
