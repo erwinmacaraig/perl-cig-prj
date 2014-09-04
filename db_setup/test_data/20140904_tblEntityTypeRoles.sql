94982885ed7e8f0eeb27ca23661ec8a8eedcf6f3
iskalot <jervy.escoto@gmail.com>
2014-09-04 07:57:00 +0000

    FC-160 test data

---
+++ db_setup/test_data/20140904_tblEntityTypeRoles.sql
@@ -0,0 +1,9 @@
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'CLUBOFFICIAL', 'PRESIDENT', 'President', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'CLUBOFFICIAL', 'VICEPRESIDENT', 'Vice President', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'CLUBOFFICIAL', 'SECRETARY', 'Secretary', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'CLUBOFFICIAL', 'TREASURER', 'Treasurer', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'MAOFFICIAL', 'MATCHCOMMISSIONER', 'Match Commissioner', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'MAOFFICIAL', 'REFEREEASSESSOR', 'Referee Assessor', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'MAOFFICIAL', 'GENERALCOORDINATOR', 'General Coordinator', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'MAOFFICIAL', 'SECURITYOFFICER', 'Security Officer', NOW());
+INSERT INTO tblEntityTypeRoles VALUES (0, 0, 0, '', 'MAOFFICIAL', 'MEDIAOFFICER', 'Media Officer', NOW());
