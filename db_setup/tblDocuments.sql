adb598c5fe6139af52032d061601ed7a2b4dcde3
facascante <f.cascante@sportingpulseinternational.com>
2014-09-05 04:27:02 +0000

    update create tblDocument db scripts

--- db_setup/tblDocuments.sql
+++ db_setup/tblDocuments.sql
@@ -1,15 +1,18 @@
 DROP TABLE IF EXISTS tblDocuments;
 
-CREATE TABLE IF NOT EXISTS tblDocuments (
-  intDocumentID INT(11) NOT NULL AUTO_INCREMENT,
-  intDocumentTypeID INT(11) NULL,
-  intEntityLevel TINYINT(4) NULL,
-  intEntityID INT(11) NOT NULL DEFAULT 0,
-  intPersonID INT(11) NOT NULL DEFAULT 0,
-  intPersonRegistrationID INT(11) NOT NULL DEFAULT 0,
-  intClearanceID INT(11) NOT NULL,
-  strDeniedNotes TEXT NULL,
-  strApprovalStatus VARCHAR(30) NOT NULL DEFAULT 'PENDING',
-  intUploadFileID INT(11) NOT NULL,
-  PRIMARY KEY (intDocumentID)
-)ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `tblDocuments` (
+  `intDocumentID` int(11) NOT NULL AUTO_INCREMENT,
+  `intDocumentTypeID` int(11) DEFAULT NULL,
+  `intEntityLevel` tinyint(4) DEFAULT NULL,
+  `intEntityID` int(11) NOT NULL DEFAULT '0',
+  `intPersonID` int(11) NOT NULL DEFAULT '0',
+  `intPersonRegistrationID` int(11) NOT NULL DEFAULT '0',
+  `intClearanceID` int(11) NOT NULL,
+  `strDeniedNotes` text,
+  `strApprovalStatus` varchar(30) NOT NULL DEFAULT 'PENDING',
+  `intUploadFileID` int(11) NOT NULL,
+  `dtAdded` datetime DEFAULT NULL,
+  `dtLastUpdated` datetime DEFAULT NULL,
+  `tTimeStamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`intDocumentID`)
+) ENGINE=InnoDB;
