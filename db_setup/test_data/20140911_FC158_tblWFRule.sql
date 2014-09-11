34bf403e79637c1a4e7f9ec2df1a0af15dfee868
iskalot <jervy.escoto@gmail.com>
2014-09-11 11:11:14 +0800

    FC-158 - rules for CLUB adding VENUE as entity

--- db_setup/test_data/20140911_FC158_tblWFRule.sql
+++ db_setup/test_data/20140911_FC158_tblWFRule.sql
@@ -9,3 +9,9 @@ INSERT INTO tblWFRule(intRealmID, intSubRealmID, intOriginLevel, strWFRuleFor, s
 
 -- ENTITY - ADDING VENUE as ENTITY (origin level = REGION; approval level = NATIONAL)
 INSERT INTO tblWFRule(intRealmID, intSubRealmID, intOriginLevel, strWFRuleFor, strEntityType, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, intApprovalEntityLevel, intProblemResolutionEntityLevel, strTaskType, strTaskStatus, tTimeStamp, intDocumentTypeID, strPersonEntityRole, strISOCountry_IN, strISOCountry_NOTIN) VALUES(1, 0, 20, 'ENTITY', '', -47, 'NEW', '', '', '', '', 100, 20, 'APPROVAL', 'ACTIVE', now(), 0, '', '', '');
+
+-- ENTITY - ADDING VENUE as ENTITY (origin level = CLUB; approval level = REGION)
+INSERT INTO tblWFRule(intRealmID, intSubRealmID, intOriginLevel, strWFRuleFor, strEntityType, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, intApprovalEntityLevel, intProblemResolutionEntityLevel, strTaskType, strTaskStatus, tTimeStamp, intDocumentTypeID, strPersonEntityRole, strISOCountry_IN, strISOCountry_NOTIN) VALUES(1, 0, 3, 'ENTITY', '', -47, 'NEW', '', '', '', '', 20, 3, 'APPROVAL', 'ACTIVE', now(), 0, '', '', '');
+
+-- ENTITY - ADDING VENUE as ENTITY (origin level = CLUB; approval level = NATIONAL)
+INSERT INTO tblWFRule(intRealmID, intSubRealmID, intOriginLevel, strWFRuleFor, strEntityType, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, intApprovalEntityLevel, intProblemResolutionEntityLevel, strTaskType, strTaskStatus, tTimeStamp, intDocumentTypeID, strPersonEntityRole, strISOCountry_IN, strISOCountry_NOTIN) VALUES(1, 0, 3, 'ENTITY', '', -47, 'NEW', '', '', '', '', 100, 3, 'APPROVAL', 'ACTIVE', now(), 0, '', '', '');
