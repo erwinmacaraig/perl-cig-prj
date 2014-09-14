e3456002af58e13a932265e87bffed2235991003
iskalot <jervy.escoto@gmail.com>
2014-09-15 07:35:12 +0800

    retained new entry

--- db_setup/test_data/20140912_FC88_tblRegoAgeRestrictions.sql
+++ db_setup/test_data/20140912_FC88_tblRegoAgeRestrictions.sql
@@ -4,3 +4,5 @@ INSERT INTO tblRegoAgeRestrictions(intRealmID,intSubRealmID,strSport,strPersonTy
 INSERT INTO tblRegoAgeRestrictions(intRealmID,intSubRealmID,strSport,strPersonType,strPersonEntityRole,strPersonLevel,strRestrictionType,strAgeLevel,intFromAge,intToAge,tTimestamp) VALUES (1,0,'FOOTBALL','TECHOFFICIAL','DOCTOR','','','ADULT',40,99,'2014-09-12 07:38:42');
 INSERT INTO tblRegoAgeRestrictions(intRealmID,intSubRealmID,strSport,strPersonType,strPersonEntityRole,strPersonLevel,strRestrictionType,strAgeLevel,intFromAge,intToAge,tTimestamp) VALUES (1,0,'FOOTBALL','CLUBOFFICIAL','PRESIDENT','','','ADULT',40,99,'2014-09-12 07:38:42');
 INSERT INTO tblRegoAgeRestrictions(intRealmID,intSubRealmID,strSport,strPersonType,strPersonEntityRole,strPersonLevel,strRestrictionType,strAgeLevel,intFromAge,intToAge,tTimestamp) VALUES (1,0,'FOOTBALL','MAOFFICIAL','MATCHCOMMISIONER','','','ADULT',50,99,'2014-09-12 07:38:42');
+
+INSERT INTO tblRegoAgeRestrictions(intRealmID, intSubRealmID, strSport, strPersonType, strPersonEntityRole, strPersonLevel, strRestrictionType, strAgeLevel, intFromAge, intToAge, tTimestamp) VALUES (1,0,'FOOTBALL','PLAYER','','AMATEUR','','ADULT',18,99,NOW());
