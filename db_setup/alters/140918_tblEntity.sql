be538783c9b2f0e2146b4a9609771f943ceb51e9
facascante <f.cascante@sportingpulseinternational.com>
2014-09-17 23:31:41 +0000

    remove schema for alter

--- db_setup/alters/140918_tblEntity.sql
+++ db_setup/alters/140918_tblEntity.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `fifasponline`.`tblEntity` 
+ALTER TABLE `tblEntity` 
 ADD COLUMN `strLegalID` VARCHAR(45) NULL COMMENT 'a field to type in the ID that corresponds to the LegalType' AFTER `strShortNotes`;
