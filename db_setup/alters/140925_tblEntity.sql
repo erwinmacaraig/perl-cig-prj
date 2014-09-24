24ae2dce87b55dffbc692895db08396839def629
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-09-25 09:42:49 +1000

    ALTER change

--- db_setup/alters/140925_tblEntity.sql
+++ db_setup/alters/140925_tblEntity.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `tblEntity` 
+ALTER TABLE `tblEntity`
 CHANGE COLUMN `intEntityLevel` `intEntityLevel` INT(11) NULL DEFAULT '0' COMMENT 'integer level of this entity that determined if it is club, region or national etc.' ,
 CHANGE COLUMN `intRealmID` `intRealmID` INT(11) NULL DEFAULT '0' COMMENT 'To which realm this entity belongs to' ,
 CHANGE COLUMN `strEntityType` `strEntityType` VARCHAR(30) NULL DEFAULT '' COMMENT 'his type represents a generic entity and its attributes.' ,
@@ -10,9 +10,9 @@ CHANGE COLUMN `strLatinShortName` `strLatinShortName` VARCHAR(100) NULL DEFAULT 
 CHANGE COLUMN `dtFrom` `dtFrom` DATE NULL DEFAULT NULL COMMENT 'The date when the Entity was founded.' ,
 CHANGE COLUMN `dtTo` `dtTo` DATE NULL DEFAULT NULL COMMENT 'The date when the Organization was dissolved or superseded by another Organization.' ,
 CHANGE COLUMN `intSubRealmID` `intSubRealmID` INT(11) NOT NULL DEFAULT '0' AFTER `intRealmID`,
-CHANGE COLUMN `strStatus` `strStatus` VARCHAR(20) NULL DEFAULT '' COMMENT 'The status of this entity. ACTIVE, INACTIVE, PENDING, SUSPENDED, DESOLVED' ,
-ADD COLUMN `strAcceptSelfRego` VARCHAR(15) NULL COMMENT 'Allow an Entity to determine if they accept self registration FC-231' AFTER `intImportID`,
-ADD COLUMN `intRealmApproved` TINYINT NULL DEFAULT 0 AFTER `strAcceptSelfRego`,
-ADD COLUMN `strPaymentNotificationAddress` VARCHAR(250) NULL AFTER `intRealmApproved`,
-ADD COLUMN `strEntityPaymentBusinessNumber` VARCHAR(100) NULL AFTER `strPaymentNotificationAddress`,
-ADD COLUMN `strEntityPaymentInfo` TEXT NULL AFTER `strEntityPaymentBusinessNumber`;
+CHANGE COLUMN `strStatus` `strStatus` VARCHAR(20) NULL DEFAULT '' COMMENT 'The status of this entity. ACTIVE, INACTIVE, PENDING, SUSPENDED, DESOLVED' ;
+ALTER TABLE tblEntity ADD COLUMN `strAcceptSelfRego` VARCHAR(15) NULL COMMENT 'Allow an Entity to determine if they accept self registration FC-231' AFTER `intImportID`,
+ALTER TABLE tblEntity ADD COLUMN `intRealmApproved` TINYINT NULL DEFAULT 0 AFTER `strAcceptSelfRego`,
+ALTER TABLE tblEntity ADD COLUMN `strPaymentNotificationAddress` VARCHAR(250) NULL AFTER `intRealmApproved`,
+ALTER TABLE tblEntity ADD COLUMN `strEntityPaymentBusinessNumber` VARCHAR(100) NULL AFTER `strPaymentNotificationAddress`,
+ALTER TABLE tblEntity ADD COLUMN `strEntityPaymentInfo` TEXT NULL AFTER `strEntityPaymentBusinessNumber`;
