84805b1d771b91c377e8e1d940b5bd0b1788c3a6
iskalot <jervy.escoto@gmail.com>
2014-09-29 09:22:06 +0800

    removed as not needed (please check db_setup/tblWFTaskNotes.sql)

--- db_setup/alters/140926_tblWFTaskNotes.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `tblWFTaskNotes` 
-DROP COLUMN `tTimeStampResolved`,
-DROP COLUMN `strResolveNotes`,
-CHANGE COLUMN `intTaskNoteID` `intTaskNoteID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
-CHANGE COLUMN `strRejectionNotes` `strNotes` VARCHAR(250) NOT NULL ,
-CHANGE COLUMN `tTimeStampRejected` `tTimeStamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
-ADD COLUMN `intParentNoteID` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Used to track which rejection note a resolution note will be mapped.' AFTER `intTaskNoteID`,
-ADD COLUMN `strType` VARCHAR(20) NULL COMMENT 'REJECT, RESOLVE, HOLD' AFTER `strNotes`;
