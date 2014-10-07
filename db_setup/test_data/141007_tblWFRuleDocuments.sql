b490540c0b4ba2879d65d35e730b000c13f1a3af
iskalot <jervy.escoto@gmail.com>
2014-10-07 15:31:07 +0800

    FC-222 - test rule documents for TRANSFERS (who can verify docs in Work Task)

---
+++ db_setup/test_data/141007_tblWFRuleDocuments.sql
@@ -0,0 +1,8 @@
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(336, 1, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(336, 2, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(337, 1, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(337, 2, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(338, 1, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(338, 2, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(339, 1, 1, 0, NOW());
+INSERT INTO tblWFRuleDocuments(intWFRuleID, intDocumentTypeID, intAllowProblemResolutionLevel, intAllowVerify, tTimeStamp) VALUES(339, 2, 1, 0, NOW());
