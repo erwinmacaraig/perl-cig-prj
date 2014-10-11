49516df30391ed4f905263aea71166945e5ae141
SP <jescoto@spdev.(none)>
2014-10-11 21:22:19 +0800

    updates on structure

--- db_setup/tblWFRuleDocuments.sql
+++ db_setup/tblWFRuleDocuments.sql
@@ -4,7 +4,10 @@ CREATE TABLE `tblWFRuleDocuments` (
     `intWFRuleDocumentID` int(11) NOT NULL AUTO_INCREMENT,
     `intWFRuleID` int(11) NOT NULL COMMENT 'The intApprovalEntityID will also approve/verify the document',
     `intDocumentTypeID` int(11) NOT NULL COMMENT 'To be checked against intDocumentTypeID in tblDocuments',
-    `intAllowProblemResolutionLevel` int(11) DEFAULT '0',
+    `intAllowApprovalEntityAdd` int(11) NOT NULL COMMENT 'Allow Approval Entity to add document',
+    `intAllowApprovalEntityVerify` int(11) NOT NULL COMMENT 'Allow Approval Entity to verify document',
+    `intAllowProblemResolutionEntityAdd` int(11) NOT NULL COMMENT 'Allow Problem Resolution Entity to add document',
+    `intAllowProblemResolutionEntityVerify` int(11) NOT NULL COMMENT 'Allow Problem Resolution Entity to verify document',
     `tTimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`intWFRuleDocumentID`),
     KEY `KEY` (`intWFRuleID`)
