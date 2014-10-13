a9731c3fe25c649d12cd8a73a12d985327515e18
iskalot <jervy.escoto@gmail.com>
2014-10-13 13:45:16 +0800

    FC-188 - registration item for player/football/professional/adult/new

---
+++ db_setup/test_data/141013_tblRegistrationItem.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO tblRegistrationItem(intRealmID, intSubRealmID, intOriginLevel, strRuleFor, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, strItemType, intID, intUseExistingThisEntity, intUseExistingAnyEntity, intRequired, tTimeStamp) VALUES(1,0,3,'REGO',3,'NEW','PLAYER','PROFESSIONAL','FOOTBALL','ADULT','DOCUMENT',3,0,0,1,NOW());
+INSERT INTO tblRegistrationItem(intRealmID, intSubRealmID, intOriginLevel, strRuleFor, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, strItemType, intID, intUseExistingThisEntity, intUseExistingAnyEntity, intRequired, tTimeStamp) VALUES(1,0,3,'REGO',3,'NEW','PLAYER','PROFESSIONAL','FOOTBALL','ADULT','DOCUMENT',4,0,0,1,NOW());
