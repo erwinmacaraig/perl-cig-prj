1aec6f8a13cb3bccd5b2f1dbc24dcf14f84da1ed
iskalot <jervy.escoto@gmail.com>
2014-10-14 16:06:23 +0800

    FC-225 - create table script (tblEmailTemplates)

---
+++ db_setup/tblEmailTemplates.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `tblEmailTemplates`;
+
+CREATE TABLE `tblEmailTemplates` (
+    `intEmailTemplateID` INT NOT NULL AUTO_INCREMENT,
+    `intEmailTemplateTypeID` INT NULL,
+    `strHTMLTemplatePath` VARCHAR(100) NULL COMMENT 'html responsive web email template',
+    `strTextTemplatePath` VARCHAR(100) NULL COMMENT 'Plain Text Email Template incase client or reader does not support html',
+    `strSubjectPrefix` VARCHAR(100) NULL COMMENT 'Prefix Email Subject',
+    `strLanguageID` INT NOT NULL COMMENT 'links to tblLanguages',
+    `intActive` INT NULL DEFAULT '1',
+    `tTimestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`intEmailTemplateID`));
