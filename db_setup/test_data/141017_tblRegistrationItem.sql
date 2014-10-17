978c695500bb1b970569635618161bfd8aad573f
iskalot <jervy.escoto@gmail.com>
2014-10-17 15:23:14 +0800

    FC-248 - added REGO document of International Player Certificate (strDocumentFor = TRANSFERITC)

---
+++ db_setup/test_data/141017_tblRegistrationItem.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO tblRegistrationItem(intRealmID, intSubRealmID, intOriginLevel, strRuleFor, intEntityLevel, strRegistrationNature, strPersonType, strPersonLevel, strSport, strAgeLevel, strItemType, intID, intUseExistingThisEntity, intUseExistingAnyEntity, intRequired, tTimeStamp) VALUES(1,0,3,'REGO',3,'NEW','PLAYER','PROFESSIONAL','FOOTBALL','ADULT','DOCUMENT',5,0,0,1,NOW());
+
+
