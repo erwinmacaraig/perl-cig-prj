4c42f784b050b094e8140bd8bc203af14d2b26f9
Bryan Posas <b.posas@sportingpulseinternational.com>
2014-10-23 07:20:35 +0000

    Fixed typo in tblEntityIdentifier.sql

--- db_setup/tblEntityIdentifier.sql
+++ db_setup/tblEntityIdentifier.sql
@@ -5,7 +5,7 @@ CREATE TABLE `tblEntityIdentifier` (
   `intRealmID` int(11) NOT NULL,
   `intIdentifierTypeID` int(11) NOT NULL,
   `strIdentifier` varchar(100) NOT NULL,
-  `strContryIssued` varchar9100) NOT NULL DEFAULT '',
+  `strContryIssued` varchar(100) NOT NULL DEFAULT '',
   `dtValidFrom` date DEFAULT NULL,
   `dtValidUntil` date DEFAULT NULL,
   `strDescription` varchar(250) DEFAULT NULL,
