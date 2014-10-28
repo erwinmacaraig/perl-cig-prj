07ceb5824dedb70611c0d807e9403f2fbeb987a4
Erwin Macaraig <e.macaraig@sportingpuseinternational.com>
2014-10-28 02:14:28 +0000

    For FC-153

--- db_setup/alters/141027_tblPerson.sql
+++ db_setup/alters/141027_tblPerson.sql
@@ -1,6 +1,12 @@
 ALTER TABLE tblPerson ADD COLUMN (
-strBirthCert varchar(20), 
-strBirthCertCountry varchar(6),
+strBirthCert varchar(20) DEFAULT '',
+strBirthCertCountry varchar(6) DEFAULT '',
 dtBirthCertValidityDateFrom date,
 dtBirthCertValidityDateTo date,
-strBirthCertDesc varchar(250));
+strBirthCertDesc varchar(250) DEFAULT '',
+strOtherPersonIdentifier varchar(20) DEFAULT '',
+strOtherPersonIdentiferIssueCountry varchar(6) DEFAULT '',
+dtOtherPersonIdentiferValidDateFrom date,
+dtOtherPersonIdentiferValidDateTo date,
+strOtherPersonIdentifierDesc varchar(250) DEFAULT ''
+);
