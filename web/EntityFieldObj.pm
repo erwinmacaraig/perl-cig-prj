fb1d8a4ff138b250d3fb05931b5a9714d5aa82d4
iskalot <jervy.escoto@gmail.com>
2014-11-04 15:55:58 +0800

    FC-224 - multiple field html elements

--- web/EntityFieldObj.pm
+++ web/EntityFieldObj.pm
@@ -4,6 +4,7 @@ use strict;
 use BaseObject;
 use Switch;
 use Defs;
+use Data::Dumper;
 our @ISA =qw(BaseObject);
 
 sub load {
@@ -46,102 +47,144 @@ sub _get_sql_details{
 
 sub fieldIDHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
     $self->getValue($field);
-    my $fieldIDhtml = "";
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldIDhtml = qq[<input type='hidden' name="$inputFieldName"/>];
     return $fieldIDhtml;
 }
 
 sub fieldNameHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldNamehtml = "";
+    $self->getValue($field);
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldNamehtml = qq[<input type='text' name='$inputFieldName'/>];
     return $fieldNamehtml;
 }
 
 sub disciplineHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldDisciplinehtml = "";
+    my $options = qq[
+        <option value="">Select Sport</option>
+    ];
+
+    my $selected = "";
+    foreach my $discipline (keys %Defs::sportType) {
+        $selected = "selected" if ($self->getValue($field) eq $discipline);
+        $options .= qq[
+            <option $selected value="$discipline">$Defs::sportType{$discipline}</option>
+        ];
+    }
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldDisciplinehtml = qq[<select name='$inputFieldName'>$options</select>];
     return $fieldDisciplinehtml;
 }
 
 sub capacityHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldCapacityhtml = "";
+    $self->getValue($field);
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldCapacityhtml = qq[<input type='text' name='$inputFieldName'/>];
     return $fieldCapacityhtml;
 }
 
 sub fieldOrderNumberHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldOrderNumberhtml = "";
+    $self->getValue($field);
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldOrderNumberhtml = qq[<input type='text' name='$inputFieldName'/>];
     return $fieldOrderNumberhtml;
 }
 
 sub groundNatureHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldGroundNaturehtml = "";
+    my $options = qq[
+        <option value="">Select Ground Nature</option>
+    ];
+
+    my $selected = "";
+    foreach my $groundNature (keys %Defs::fieldGroundNatureType) {
+        $selected = "selected" if ($self->getValue($field) eq $groundNature);
+        $options .= qq[
+            <option $selected value="$groundNature">$Defs::fieldGroundNatureType{$groundNature}</option>
+        ];
+    }
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldGroundNaturehtml = qq[<select name='$inputFieldName'>$options</select>];
     return $fieldGroundNaturehtml;
 }
 
 sub lengthHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldLengthhtml = "";
+    $self->getValue($field);
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldLengthhtml = qq[<input type='text' name='$inputFieldName'/>];
     return $fieldLengthhtml;
 }
 
 sub widthHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
-    my $fieldWidthhtml = "";
+    $self->getValue($field);
+    my $inputFieldName = $self->_inputFieldName($field, $prefixID);
+    my $fieldWidthhtml = qq[<input type='text' name='$inputFieldName'/>];
     return $fieldWidthhtml;
 }
 
 sub getHtml {
     my $self = shift;
-    my ($field) = @_;
+    my ($field, $prefixID) = @_;
 
     switch($field){
         case 'intEntityFieldID' {
-            return $self->fieldNameHtml($field);
+            return $self->fieldIDHtml($field, $prefixID);
         }
         case 'intFieldOrderNumber' {
-            return $self->fieldOrderNumberHtml($field);
+            return $self->fieldOrderNumberHtml($field, $prefixID);
         }
         case 'strName' {
-            return $self->fieldNameHtml($field);
+            return $self->fieldNameHtml($field, $prefixID);
         }
         case 'strDiscipline' {
-            return $self->disciplineHtml($field);
+            return $self->disciplineHtml($field, $prefixID);
         }
         case 'intCapacity' {
-            return $self->capacityHtml($field);
+            return $self->capacityHtml($field, $prefixID);
         }
         case 'strGroundNature' {
-            return $self->groundNatureHtml($field);
+            return $self->groundNatureHtml($field, $prefixID);
         }
         case 'dblLength' {
-            return $self->lengthHtml($field);
+            return $self->lengthHtml($field, $prefixID);
         }
         case 'dblWidth' {
-            return $self->widthHtml($field);
+            return $self->widthHtml($field, $prefixID);
         }
         else {
             return;
         }
     }
+}
+
+sub _inputFieldName {
+    my $self = shift;
+    my ($field, $prefixID) = @_;
+
+    return $field . "_" . $prefixID;
 }
 
 1;
