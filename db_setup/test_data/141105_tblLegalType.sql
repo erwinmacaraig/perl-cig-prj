f9fc9d66eb506d270b8c9c7a12522473df90dea8
iskalot <jervy.escoto@gmail.com>
2014-11-05 16:50:03 +0800

    FC-223 - test data for legal entity type

---
+++ db_setup/test_data/141105_tblLegalType.sql
@@ -0,0 +1,5 @@
+INSERT INTO tblLegalType VALUES(0, 1, 'Association', NOW());
+INSERT INTO tblLegalType VALUES(0, 1, 'Private Ltd', NOW());
+INSERT INTO tblLegalType VALUES(0, 1, 'Incorporated', NOW());
+INSERT INTO tblLegalType VALUES(0, 1, 'Foundation', NOW());
+INSERT INTO tblLegalType VALUES(0, 1, 'Other', NOW());
