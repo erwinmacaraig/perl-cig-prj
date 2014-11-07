ba9210dfe7e07f22e0615b3dd5e016538954227d
iskalot <jervy.escoto@gmail.com>
2014-11-07 13:37:55 +0800

    FC-270 - sys config for locking payment per type

---
+++ db_setup/test_data/141107_tblSystemConfig.sql
@@ -0,0 +1,4 @@
+INSERT INTO tblSystemConfig VALUES (0,1,'lockApproval_PaymentRequired_REGO', 1, NOW(), 1,0);
+INSERT INTO tblSystemConfig VALUES (0,1,'lockApproval_PaymentRequired_CLUB', 1, NOW(), 1,0);
+INSERT INTO tblSystemConfig VALUES (0,1,'lockApproval_PaymentRequired_VENUE', 1, NOW(), 1,0);
+INSERT INTO tblSystemConfig VALUES (0,1,'lockApproval_PaymentRequired_PERSON', 1, NOW(), 1,0);
