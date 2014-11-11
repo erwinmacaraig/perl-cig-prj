5984990e1f09114dd6a81f52dc14cc8373ebb98d
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-11-11 11:14:56 +1100

    Request Below report

--- web/Reports/ReportAdvanced_ClearancesAllBelow.pm
+++ web/Reports/ReportAdvanced_ClearancesAllBelow.pm
@@ -1,24 +1,105 @@
 #
-# $Header: svn://svn/SWM/trunk/web/Reports/ReportAdvanced_ClearancesAllBelow.pm 8251 2013-04-08 09:00:53Z rlee $
+# $Header: svn://svn/SWM/trunk/web/Reports/ReportAdvanced_Clearances.pm 8849 2013-07-04 02:14:36Z dhanslow $
 #
 
-package Reports::ReportAdvanced_ClearancesAllBelow;
+package Reports::ReportAdvanced_Clearances;
 
 use strict;
 use lib ".";
 use ReportAdvanced_Common;
 use Reports::ReportAdvanced;
-use Reports::ReportAdvanced_Clearances;
-our @ISA =qw(Reports::ReportAdvanced_Clearances);
+our @ISA =qw(Reports::ReportAdvanced);
 
 
 use strict;
 
 sub _getConfiguration {
-  my $self = shift;
+	my $self = shift;
 
-  $self->SUPER::_getConfiguration();
-  $self->{'Config'}{'SQLBuilder'} = \&SQLBuilder;
+	my $currentLevel = $self->{'EntityTypeID'} || 0;
+	my $Data = $self->{'Data'};
+	my $SystemConfig = $self->{'SystemConfig'};
+	my $clientValues = $Data->{'clientValues'};
+
+  my $txt_Clr = $SystemConfig->{'txtCLR'} || 'Clearance';
+	my $showAgentFields = ($SystemConfig->{'clrHide_AgentFields'} == 1) ? '0' : '1';
+  my $natnumname=$SystemConfig->{'NationalNumName'} || 'National Number';
+
+  my $CommonVals = getCommonValues(
+    $Data,
+    {
+      DefCodes => 1,
+    },
+  );
+
+	my %config = (
+		Name => "Request Report",
+
+		StatsReport => 0,
+		MemberTeam => 0,
+		ReportEntity => 3,
+		ReportLevel => 0,
+		Template => 'default_adv',
+		TemplateEmail => 'default_adv_CSV',
+		DistinctValues => 1,
+    SQLBuilder => \&SQLBuilder,
+
+		Fields => {
+            intPersonRequestID=> ["Request No.",{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1}],
+            strRequestType=> ['Request Type', { displaytype=>'lookup', fieldtype=> 'dropdown', dropdownoptions => \%Defs::personRequest, allowgrouping => 1}],
+            strNationalNum=> [$natnumname,{displaytype=>'text', fieldtype=>'text', allowsort=>1}, active=>1],
+            strLocalFirstname=> ["First name",{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1}],
+            strLocalSurname=> ["Family name",{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1}],
+            strLatinFirstname=> ["International First name",{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1}],
+            strLatinSurname=> ["International Family name",{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1}],
+            dtDOB=> ['Date of Birth',{displaytype=>'date', fieldtype=>'date', dbfield=>'M.dtDOB', dbformat=>' DATE_FORMAT(P.dtDOB,"%d/%m/%Y")'}, active=>1],
+            dtYOB=> ['Year of Birth',{displaytype=>'date', fieldtype=>'text', allowgrouping=>1, allowsort=>1, dbfield=>'YEAR(P.dtDOB)', dbformat=>' YEAR(M.dtDOB)'}],
+            SourceClubName=> ['Source Club',{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1, dbfield => 'SourceClub.strLocalName', allowgrouping=>1}],
+            DestinationClubName=> ['Destination Club',{displaytype=>'text', fieldtype=>'text', active=>1, allowsort=>1, dbfield => 'DestinationClub.strLocalName', allowgrouping=>1}],
+
+            strSport=> ['Registration Sport', { displaytype=> 'lookup', fieldtype=> 'dropdown', dropdownoptions => \%Defs::sportType, allowgrouping=> 1 }],
+            strPersonEntityRole=> [ 'Sub Role', { displaytype=>'text', fieldtype=>'text'}],
+            strPersonType=> ['Registration Role', { displaytype=>'lookup', fieldtype=> 'dropdown', dropdownoptions => \%Defs::personType, allowgrouping => 1}],
+            strPersonLevel=> ['Registration Level', { displaytype=> 'lookup', fieldtype=> 'dropdown', dropdownoptions => \%Defs::personLevel, allowgrouping=>1}],
+            dtDateRequest=> ['Date Requested', {active=>1, displaytype=>'date', fieldtype=>'datetime', allowsort=>1, dbformat=>' DATE_FORMAT(PRQ.dtDateRequest,"%d/%m/%Y %H:%i")', }],
+            strRequestStatus => ["Request Status" ,{displaytype=>'lookup', active=>1, fieldtype=>'dropdown', dropdownoptions => \%Defs::personRequestStatus, allowsort=>1, allowgrouping=>1}],
+		},
+
+		Order => [qw(
+			intPersonRequestID
+			strNationalNum
+			strLocalFirstname
+			strLocalSurname
+			strLatinFirstname
+			strLatinSurname
+			dtDOB
+			dtYOB
+			SourceClubName
+			DestinationClubName
+            strRequestType
+            strSport
+            strPersonType
+            strPersonLevel
+            strPersonEntityRole
+            strRequestStatus
+			dtDateRequest
+		)],
+    OptionGroups => {
+      default => ['Details',{}],
+    },
+
+		Config => {
+			FormFieldPrefix => 'c',
+			FormName => 'clearform_',
+			EmailExport => 1,
+			limitView  => 5000,
+			EmailSenderAddress => $Defs::admin_email,
+			SecondarySort => 1,
+			RunButtonLabel => 'Run Report',
+			ReturnProcessData => [qw(tblTeam.strEmail tblTeam.strName)],
+		},
+	);
+	$self->{'Config'} = \%config;
 }
 
 sub SQLBuilder  {
@@ -41,48 +122,33 @@ sub SQLBuilder  {
 
     $where_list=' AND '.$where_list if $where_list and ($where_levels or $current_where);
 
-	my $tempWHERE = '';
-	$tempWHERE = qq[ (Temp1.int10_ID = $self->{'EntityID'} or Temp2.int10_ID = $self->{'EntityID'}) ] if ($currentLevel == 10);
-	$tempWHERE = qq[ (Temp1.int20_ID = $self->{'EntityID'} or Temp2.int20_ID = $self->{'EntityID'}) ] if ($currentLevel == 20);
-	$tempWHERE = qq[ (Temp1.int30_ID = $self->{'EntityID'} or Temp2.int30_ID = $self->{'EntityID'}) ] if ($currentLevel == 30);
-	$tempWHERE = qq[ (Temp1.int100_ID = $self->{'EntityID'} or Temp2.int100_ID = $self->{'EntityID'}) ] if ($currentLevel == 100);
-
     $sql = qq[
       SELECT 
-				C.*,
-				DATE_FORMAT(C.dtApplied, "%d/%m/%Y") AS dtApplied,
-				CP.intClearanceStatus as PathStatus,
-				CP.intClearancePathID,
-				M.strSurname,
-				M.strFirstname,
-				SourceClub.strName as SourceClubName,
-				DestinationClub.strName as DestinationClubName,
-				SourceAssoc.strName as SourceAssocName,
-				DestinationAssoc.strName as DestinationAssocName,
-				DATE_FORMAT(CP.dtAlert, "%d/%m/%Y") AS dtAlert,
-				IF(dtAlert <> '00/00/0000', 1, 0) as AlertType,
-				IF(dtAlert <= NOW(), 1, 0) as AlertNow,
-				M.strNationalNum,
-				DATE_FORMAT(M.dtDOB, "%d/%m/%Y") as dtDOB,
-				DATE_FORMAT(M.dtDOB, "%Y") as dtYOB
-				FROM tblClearance as C
-					LEFT JOIN tblClearancePath as CP ON (
-						CP.intClearanceID = C.intClearanceID AND 
-						CP.intClearancePathID = C.intCurrentPathID)
-					INNER JOIN tblMember as M ON (M.intMemberID = C.intMemberID)
-					INNER JOIN tblAssoc as SourceAssoc ON (SourceAssoc.intAssocID = C.intSourceAssocID)
-					INNER JOIN tblAssoc as DestinationAssoc ON (DestinationAssoc.intAssocID = C.intDestinationAssocID)
-					LEFT JOIN tblClub as SourceClub ON (SourceClub.intClubID = C.intSourceClubID)
-					LEFT JOIN tblClub as DestinationClub ON (DestinationClub.intClubID = C.intDestinationClubID)
-					INNER JOIN tblTempNodeStructure as Temp1 ON (Temp1.intAssocID = C.intSourceAssocID)
-					INNER JOIN tblTempNodeStructure as Temp2 ON (Temp2.intAssocID = C.intDestinationAssocID)
+				PRQ.*,
+				DATE_FORMAT(PRQ.dtDateRequest, "%d/%m/%Y") AS dtDateRequest,
+				P.strLocalSurname,
+				P.strLocalFirstname,
+				P.strLatinSurname,
+				P.strLatinFirstname,
+				SourceClub.strLocalName as SourceClubName,
+				DestinationClub.strLocalName as DestinationClubName,
+				P.strNationalNum,
+				DATE_FORMAT(P.dtDOB, "%d/%m/%Y") as dtDOB,
+				DATE_FORMAT(P.dtDOB, "%Y") as dtYOB
+				FROM tblPersonRequest as PRQ
+					INNER JOIN tblPerson as P ON (P.intPersonID = PRQ.intPersonID)
+					LEFT JOIN tblEntity as SourceClub ON (SourceClub.intEntityID = PRQ.intRequestFromEntityID)
+					LEFT JOIN tblEntity as DestinationClub ON (DestinationClub.intEntityID = PRQ.intRequestToEntityID)
+                    LEFT JOIN tblTempEntityStructure as TempEntFrom ON (TempEntFrom.intChildID=PRQ.intRequestFromEntityID)
+                    LEFT JOIN tblTempEntityStructure as TempEntTo ON (TempEntTo.intChildID=PRQ.intRequestToEntityID)
 				WHERE 
-					$tempWHERE
-					AND C.intRecStatus <> -1
-					AND C.intCreatedFrom = 0
+                    PRQ.intRealmID = $Data->{'Realm'}
+					AND (
+                        TempEntFrom.intParentID= $self->{'EntityID'} 
+                        OR TempEntTo.intParentID= $self->{'EntityID'}
+                    )
 					$where_list
     ];
-	print STDERR $sql;
     return ($sql,'');
   }
 }
