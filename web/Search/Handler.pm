08152074c0a49bb5e60be91ed61e3903551dc546
iskalot <jervy.escoto@gmail.com>
2014-11-12 18:35:15 +0800

    FC-71 - searchobj instantiation

--- web/Search/Handler.pm
+++ web/Search/Handler.pm
@@ -12,14 +12,31 @@ sub handle {
     my ($action, $Data) = @_;
 
     my $searchObj = undef;
+    my $searchType = '';
 
     switch ($action) {
         case 'INITSRCH_P' {
-            $searchObj = undef;
+            $searchType = "INITSRCH_P";
+            $searchObj = new Search::Person();
+        }
+        case 'INITSRCH_C' {
+            return;
+        }
+        case 'INITSRCH_V' {
+            return;
         }
         else {
             return;
         }
     }
 
+    $searchObj
+        ->setRealmID($Data->{'Realm'})
+        ->setSubRealmID(0)
+        ->setSearchType($searchType)
+        ->setData($Data);
+
+    
+
+    return $searchObj->displaySearchForm();
 }
