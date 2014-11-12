e987e2994f5489764191de1960dec0a7c2eb51b1
iskalot <jervy.escoto@gmail.com>
2014-11-12 18:36:10 +0800

    FC-71 - abstract methods to use

---
+++ web/Search/Person.pm
@@ -0,0 +1,18 @@
+package Search::Person;
+
+use strict;
+use lib '.', '..', '../..';
+use Search::Base;
+our @ISA = qw(Search::Base);
+
+use Data::Dumper;
+
+sub search {
+
+}
+
+sub processResult {
+
+}
+
+1;
