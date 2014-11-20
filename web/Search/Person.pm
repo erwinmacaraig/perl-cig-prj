a6f4d06519748c5e3be394f9f90f71cc9632d615
iskalot <jervy.escoto@gmail.com>
2014-11-20 21:34:32 +0800

    FC-71 - person rego details on search result

--- web/Search/Person.pm
+++ web/Search/Person.pm
@@ -20,7 +20,6 @@ sub process {
     #set filters here based on search type
     #ie transfer, access, unique otherwise default
 
-    print STDERR Dumper "SEARCH TYPE " . $searchType;
     switch($searchType){
         case 'unique' {
             return $self->getUnique($raw);
@@ -33,7 +32,7 @@ sub process {
             return;
         }
         case 'default' {
-            return $self->getUnique($raw);
+            return $self->getPersonRegistration($raw);
         }
         else {
             return $self->getUnique($raw);
@@ -235,7 +234,112 @@ sub getTransfer {
 
 }
 
-sub getRegistration {
+sub getPersonRegistration {
+    my ($self) = shift;
+    my ($raw) = @_;
+
+    $self->setGridTemplate("search/grid/personregistration.templ");
+
+    my ($intermediateNodes, $subNodes) = $self->getIntermediateNodes();
+    my $filters = $self->setupFilters($subNodes);
+
+    my $realmID = $self->getData()->{'Realm'};
+    $self->getSphinx()->ResetFilters();
+    $self->getSphinx()->SetFilter('intrealmid', [$filters->{'realm'}]);
+
+    #exclude persons that are already in the CLUB initiating the transfer
+    $self->getSphinx()->SetFilter('intentityid', $filters->{'entity'}) if $filters->{'entity'};
+    my $results = $self->getSphinx()->Query($self->getKeyword(), 'FIFA_Persons_r'.$filters->{'realm'});
+    my @persons = ();
+
+    if($results and $results->{'total'})  {
+        for my $r (@{$results->{'matches'}})  {
+            push @persons, $r->{'doc'};
+        }
+    }
+
+    my @memarray = ();
+    if(@persons)  {
+        my $person_list = join(',',@persons);
+
+        my $entity_list = '';
+        $entity_list = join(',', @{$subNodes});
+
+        my $clubID = $self->getData()->{'clientValues'}{'clubID'} || 0;
+        $clubID = 0 if $clubID == $Defs::INVALID_ID;
+
+        my $st = qq[
+            SELECT DISTINCT
+                tblPerson.intPersonID,
+                tblPerson.strLocalFirstname,
+                tblPerson.strLocalSurname,
+                tblPerson.strNationalNum,
+                tblPerson.strFIFAID,
+                tblPerson.dtDOB,
+                PR.intPersonRegistrationID,
+                PR.strPersonType,
+                PR.strSport,
+                E.strLocalName AS EntityName,
+                E.intEntityID,
+                E.intEntityLevel
+            FROM
+            tblPerson
+            INNER JOIN tblPersonRegistration_$realmID AS PR ON (
+                tblPerson.intPersonID = PR.intPersonID
+                AND PR.strStatus IN ('ACTIVE', 'PASSIVE','PENDING')
+                AND PR.intEntityID IN ($entity_list)
+            )
+            INNER JOIN tblEntity AS E ON (
+                PR.intEntityID = E.intEntityID
+            )
+            WHERE tblPerson.intPersonID IN ($person_list)
+            ORDER BY 
+                tblPerson.strLocalSurname,
+                tblPerson.strLocalFirstname,
+                tblPerson.strNationalNum
+        ];
+        my $q = $self->getData->{'db'}->prepare($st);
+        $q->execute();
+        my %origClientValues = %{$self->getData()->{'clientValues'}};
+
+        my $count = 0;
+        my $target = $self->getData()->{'target'};
+        my $client = $self->getData()->{'client'};
+        while(my $dref = $q->fetchrow_hashref()) {
+            my $link = $self->getSearchLink(
+                $self->getData(),
+                $Defs::LEVEL_PERSON,
+                '',
+                $dref->{'intPersonID'},
+                $intermediateNodes,
+                $dref->{'intEntityID'},
+                $dref->{'intEntityLevel'},
+            );
+
+            $count++;
+            my $name = "$dref->{'strLocalFirstname'} $dref->{'strLocalSurname'}" || '';
+            push @memarray, {
+                id => $dref->{'intPersonID'} || next,
+                ma_id => $dref->{'strNationalNum'},
+                link => $link,
+                name => $name,
+                dob => $dref->{'dtDOB'},
+                role => $Defs::personType{$dref->{'strPersonType'}},
+            };
+        }
+
+        if($raw){
+            return \@memarray;
+        }
+        else {
+            return $self->displayResultGrid(\@memarray) if $count;
+
+            return $count;
+        }
+
+    }
+
+    return;
 
 }
 
