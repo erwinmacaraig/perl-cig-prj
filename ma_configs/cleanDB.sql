e1dd96552125cc6d388b073b47886902248c16f8
root <root@ip-172-31-29-82.eu-west-1.compute.internal>
2014-12-09 03:45:07 +0000

    Delete scripts

---
+++ ma_configs/cleanDB.sql
@@ -0,0 +1,16 @@
+DELETE FROM tblEntity WHERE intEntityID<>1;
+DELETE FROM tblUserAuth WHERE entityId>1;
+DELETE FROM tblPerson;
+DELETE FROM tblPersonRegistration_1;
+DELETE FROM tblPersonRequest;
+DELETE FROM tblDocuments;
+DELETE FROM tblTransactions;
+DELETE FROM tblTXNLog;
+DELETE FROM tblTransLog;
+DELETE FROM tblPersonCertifications;
+DELETE FROM tblEntityFields;
+DELETE FROM tblPersonNotes;
+DELETE FROM tblPlayerPassport;
+DELETE FROM tblTransLog_Retry;
+DELETE FROM tblTransLog_Counts;
+
