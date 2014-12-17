28eeab0b0b920bb3bec67823e3683a38419cdde6
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-12-18 09:38:13 +1100

    Club Products

---
+++ db_setup/test_data/141218_clubProducts.sql
@@ -0,0 +1,7 @@
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',9,0,0,NOW(),0,'','','');
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',10,0,0,NOW(),0,'','','');
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',11,0,0,NOW(),0,'','','');
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',12,0,0,NOW(),0,'','','');
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',13,0,0,NOW(),0,'','','');
+INSERT INTO tblRegistrationItem VALUES (0,1,0,100,'ENTITY','',3,'NEW','','','','','PRODUCT',14,0,0,NOW(),0,'','','');
+
