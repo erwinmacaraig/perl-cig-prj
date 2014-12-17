7b4f354cee4af0643d7d62223126ab4d832ea8b5
iskalot <jervy.escoto@gmail.com>
2014-12-17 11:30:54 +0800

    display uploaded documents; for edit link on Summary page

--- web/PersonRegistrationFlow_Common.pm
+++ web/PersonRegistrationFlow_Common.pm
@@ -545,22 +545,50 @@ sub displayRegoFlowDocuments{
         $rego_ref,
      );
 	my @docos = (); 
+
+    my %existingDocuments;
 	#check for uploaded documents present for a particular registration and person
-	my $query = qq[
-					SELECT distinct(tblDocuments.intDocumentTypeID), tblDocumentType.strDocumentName
-					FROM tblDocuments 
-						INNER JOIN tblDocumentType
-					ON tblDocuments.intDocumentTypeID = tblDocumentType.intDocumentTypeID
-					INNER JOIN tblRegistrationItem 
-					ON tblDocumentType.intDocumentTypeID = tblRegistrationItem.intID
-					WHERE tblDocuments.intPersonID = ? AND intPersonRegistrationID = ?;	
-	];
+    #my $query = qq[
+	#				SELECT distinct(tblDocuments.intDocumentTypeID), tblDocumentType.strDocumentName
+	#				FROM tblDocuments 
+	#					INNER JOIN tblDocumentType
+	#				ON tblDocuments.intDocumentTypeID = tblDocumentType.intDocumentTypeID
+	#				INNER JOIN tblRegistrationItem 
+	#				ON tblDocumentType.intDocumentTypeID = tblRegistrationItem.intID
+	#				WHERE tblDocuments.intPersonID = ? AND intPersonRegistrationID = ?;	
+	#];
    
+    my $query = qq [
+        SELECT
+        tblDocuments.intDocumentTypeID as ID,
+        tblRegistrationItem.intUseExistingThisEntity as UseExistingThisEntity,
+        tblRegistrationItem.intUseExistingAnyEntity as UseExistingAnyEntity,
+        tblUploadedFiles.strOrigFilename,
+        tblUploadedFiles.intFileID,
+        tblUploadedFiles.intAddedByTypeID as AddedByTypeID,
+        tblDocumentType.strDocumentName as Name,
+        tblDocumentType.strDescription as Description
+
+        FROM tblDocuments
+        INNER JOIN tblDocumentType
+            ON tblDocuments.intDocumentTypeID = tblDocumentType.intDocumentTypeID
+        INNER JOIN tblRegistrationItem 
+            ON tblDocumentType.intDocumentTypeID = tblRegistrationItem.intID 
+        INNER JOIN tblUploadedFiles
+            ON tblUploadedFiles.intFileID = tblDocuments.intUploadFileID 
+        AND tblDocuments.intPersonID = ?
+        AND tblDocuments.intPersonRegistrationID = ?
+        ORDER BY tblDocuments.intDocumentID DESC
+    ];
+
 	my $sth = $Data->{'db'}->prepare($query);
 	$sth->execute($personID,$regoID);
 	my @uploaded_docs = ();
 	while(my $dref = $sth->fetchrow_hashref()){		
-		push @uploaded_docs, $dref->{'intDocumentTypeID'};		
+        #push @uploaded_docs, $dref->{'intDocumentTypeID'};		
+        if(! exists $existingDocuments{$dref->{'ID'}}){
+            $existingDocuments{$dref->{'ID'}} = $dref;
+        }
 	}
 	
 	my @diff = ();	
@@ -604,7 +632,7 @@ sub displayRegoFlowDocuments{
             ON tblDocumentType.intDocumentTypeID = tblRegistrationItem.intID 
         INNER JOIN tblUploadedFiles
             ON tblUploadedFiles.intFileID = tblDocuments.intUploadFileID 
-        WHERE strApprovalStatus = 'APPROVED'
+        WHERE strApprovalStatus IN ('APPROVED', 'PENDING')
         AND intPersonID = ?
         AND (tblRegistrationItem.intUseExistingThisEntity = 1 OR tblRegistrationItem.intUseExistingAnyEntity = 1) 
         ORDER BY tblDocuments.intDocumentID DESC
@@ -613,7 +641,6 @@ sub displayRegoFlowDocuments{
 	$sth = $Data->{'db'}->prepare($query);
 	$sth->execute($personID);
 
-    my %existingDocuments;
 	while(my $dref = $sth->fetchrow_hashref()){
         if(! exists $existingDocuments{$dref->{'ID'}}){
             $existingDocuments{$dref->{'ID'}} = $dref;
@@ -642,7 +669,6 @@ sub displayRegoFlowDocuments{
     	
     }
 
-    print STDERR Dumper @required_docs_listing;
     #if (! scalar @required_docs_listing and ! scalar @optional_docs_listing)  {
      #   return '';
     #}
