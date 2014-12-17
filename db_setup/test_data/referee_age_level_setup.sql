b3321e7798c334bfb5d070d9028b973e159a89c9
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-12-18 06:35:56 +1100

    Renewal

--- db_setup/test_data/referee_age_level_setup.sql
+++ db_setup/test_data/referee_age_level_setup.sql
@@ -5,6 +5,11 @@ INSERT INTO tblMatrix(intMatrixID, intRealmID, intSubRealmID, intEntityLevel, st
 
 -- update to MINOR
 UPDATE tblMatrix SET strAgeLevel = 'MINOR' WHERE intMatrixID = 97;
+--Renew
+INSERT INTO tblMatrix(intMatrixID, intRealmID, intSubRealmID, intEntityLevel, strWFRuleFor, strEntityType, strPersonType, strRegistrationNature, strPersonLevel, strSport, intOriginLevel, strAgeLevel, intPaymentRequired, dtAdded, tTimeStamp, intOfEntityLevel, strPersonEntityRole, intLocked, dtFrom, dtTo) SELECT 0, intRealmID, intSubRealmID, intEntityLevel, strWFRuleFor, strEntityType, strPersonType, strRegistrationNature, strPersonLevel, strSport, intOriginLevel, 'ADULT', intPaymentRequired, dtAdded, tTimeStamp, intOfEntityLevel, strPersonEntityRole, intLocked, dtFrom, dtTo FROM tblMatrix WHERE intMatrixID = 98;
+-- update to MINOR
+UPDATE tblMatrix SET strAgeLevel = 'MINOR' WHERE intMatrixID = 98;
+
 
 -- below should satisfy REFEREE age range of 16-45, including age level
 -- add MINOR REFEREE with age from 16-20 
