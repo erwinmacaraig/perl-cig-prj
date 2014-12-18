e231c5881c1c2279bf3df6f91fc5dee3da2c33c7
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-12-19 09:38:10 +1100

    SQL

--- web/WorkFlow.pm
+++ web/WorkFlow.pm
@@ -2424,7 +2424,7 @@ sub populateDocumentViewData {
 	my $query = qq[SELECT tblDocuments.intDocumentTypeID, tblDocuments.intUploadFileID FROM tblDocuments INNER JOIN tblDocumentType
 				ON tblDocuments.intDocumentTypeID = tblDocumentType.intDocumentTypeID INNER JOIN tblRegistrationItem 
 				ON tblDocumentType.intDocumentTypeID = tblRegistrationItem.intID 
-				WHERE strApprovalStatus = 'APPROVED' AND intPersonID = ? AND tblRegistrationItem.intRealmID=? AND 
+				WHERE strApprovalStatus IN('PENDING', 'APPROVED') AND intPersonID = ? AND tblRegistrationItem.intRealmID=? AND 
 				(tblRegistrationItem.intUseExistingThisEntity = 1 OR tblRegistrationItem.intUseExistingAnyEntity = 1) 
 				GROUP BY intDocumentTypeID];
 	my $sth = $Data->{'db'}->prepare($query);
