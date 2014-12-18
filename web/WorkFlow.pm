0537b9f38e9e9cdff7e157a0ff203854f40b775a
Bruce Irvine <bruce.g.irvine@gmail.com>
2014-12-19 10:14:55 +1100

    SQL

--- web/WorkFlow.pm
+++ web/WorkFlow.pm
@@ -2533,7 +2533,6 @@ sub populateDocumentViewData {
         ORDER BY dt.strDocumentName, d.intDocumentID DESC
     ];
    
-print STDERR $st;
     my $q = $Data->{'db'}->prepare($st) or query_error($st);
     $q->execute(
         $dref->{'intWFTaskID'},
