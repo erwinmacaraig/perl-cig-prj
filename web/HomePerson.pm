9f77737770e8517b66c48b9cdb93c23162353ca2
aaronlague <aaronlague@yahoo.com>
2014-12-24 14:45:51 +0800

    FC-613 - UI of Documents for Active users

--- web/HomePerson.pm
+++ web/HomePerson.pm
@@ -204,15 +204,9 @@ sub showPersonHome	{
 
 		if($fileID) {
 			$displayView = 1;
-            $viewLink = qq[ <span style="position: relative"> 
-<a href="#" class="btn-inside-docs-panel" onclick="docViewer($fileID,'client=$clm&amp;a=view');return false;">]. $Data->{'lang'}->txt('View') . q[</a></span>];		
-
-				
+            $viewLink = qq[ <span style="position: relative"><a href="#" class="btn-inside-docs-panel" onclick="docViewer($fileID,'client=$clm&amp;a=view');return false;">]. $Data->{'lang'}->txt('View') . q[</a></span>];
         }
-		#$replaceLink = qq[ <span style="position: relative"><a href="#" class="btn-inside-docs-panel" onclick="replaceFile($fileID,$doc->{'intDocumentTypeID'}, $rego->{'intPersonRegistrationID'}, $personID, '$clm', '$documentName', ' ');return false;">]. $Data->{'lang'}->txt('Replace') . q[</a></span>]; 
-		$replaceLink = qq[ <span style="position: relative"><a href="#" class="btn-inside-docs-panel" onclick="replaceFile($fileID,'$parameters','$documentName','');return false;"> ]. $Data->{'lang'}->txt('Replace') . q[</a></span>];	
-
-		#$addLink = qq[ <a href="#" class="btn-inside-docs-panel" onclick="replaceFile(0,$doc->{'intDocumentTypeID'}, $rego->{'intPersonRegistrationID'}, $personID, '$clm','$documentName',' ');return false;">]. $Data->{'lang'}->txt('Add') . q[</a>] if (!$Data->{'ReadOnlyLogin'});
+		$replaceLink = qq[ <span style="position: relative"><a href="#" class="btn-inside-docs-panel" onclick="replaceFile($fileID,'$parameters','$documentName','');return false;">]. $Data->{'lang'}->txt('Replace') . q[</a></span>];
 		$addLink = qq[ <a href="#" class="btn-inside-docs-panel" onclick="replaceFile(0,'$parameters','$documentName','');return false;">]. $Data->{'lang'}->txt('Add') . q[</a>] if (!$Data->{'ReadOnlyLogin'});
 
 		#push @alldocs, { . " - $rego->{intPersonRegistrationID} "
