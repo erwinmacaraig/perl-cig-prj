002f2916937839353f3ed75e5fab1bd54bfd5a34
iskalot <jervy.escoto@gmail.com>
2014-12-26 18:41:31 +0800

    FC-515 - display Member ID as PENDING if was registered and has a active approval for amendment

--- web/PersonSummaryPanel.pm
+++ web/PersonSummaryPanel.pm
@@ -47,7 +47,7 @@ sub personSummaryPanel {
     my $isocountries  = getISOCountriesHash();
     my %templateData = (
         #'NationalNum' => $personObj->getValue('strNationalNum') || '',
-        'NationalNum' => $personObj->getValue('strStatus') eq $Defs::PERSON_STATUS_REGISTERED ? $personObj->getValue('strNationalNum') || '' : '',
+        'NationalNum' => $personObj->getValue('strStatus') eq $Defs::PERSON_STATUS_REGISTERED ? $personObj->getValue('strNationalNum') || '' : $personObj->getValue('strNationalNum') ? 'PENDING' : '',
         'FamilyName' => $personObj->getValue('strLocalSurname') || '',
         'FirstName' => $personObj->getValue('strLocalFirstname') || '',
         'dob' => $personObj->getValue('dtDOB'),
