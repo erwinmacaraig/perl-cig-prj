d020697eb21b54c35aaf284f27f357dfeea8557e
Bruce Irvine <bruce.g.irvine@gmail.com>
2015-01-02 15:25:23 +1100

    Products

--- web/Products.pm
+++ web/Products.pm
@@ -2458,6 +2458,35 @@ sub apply_product_rules {
         $Data->{'db'}->do($query2);
     }
 
+   {
+
+        my $st = qq[
+            UPDATE tblTransactions as T
+                INNER JOIN tblPersonRegistration_$Data->{'Realm'} as PR ON ( PR.intPersonRegistrationID = T.intPersonRegistrationID)
+                INNER JOIN tblNationalPeriod as NP ON (NP.intNationalPeriodID = PR.intNationalPeriodID)
+            SET
+                T.dtStart = NP.dtFrom,
+                T.dtEnd= NP.dtTo
+            WHERE
+                T.intTransLogID= ?
+                AND T.intRealmID = ?
+                AND (
+                    NOT T.dtStart
+                    OR T.dtStart IS NULL
+                    OR T.dtStart = '0000-00-00'
+                )
+				AND T.intTableType = $Defs::LEVEL_PERSON
+				AND T.intID = ?
+        ];
+
+		my $q = $Data->{'db'}->prepare($st);
+		$q->execute(
+		    $transID,
+    		$Data->{'Realm'},
+            $personID
+		);
+    }
+
     warn("PERSON REGO RECORD HERE ?");
 
 }
