6c502516d4f6c3b0667d6dc6274b522ec2884244
Bruce Irvine <bruce.g.irvine@gmail.com>
2015-01-08 16:52:53 +1100

    Work on Importer

--- importer/ImporterConfig.pm
+++ importer/ImporterConfig.pm
@@ -2,9 +2,9 @@ package ImporterConfig;
 require Exporter;
 @ISA =  qw(Exporter);
 %DB_CONFIG = (
-		"DB_DSN" => "DBI:mysql:fifasponline_demo", #fifasponline
+		"DB_DSN" => "DBI:mysql:fifaSingaporeTest", #fifasponline
 		"DB_USER" => "root",
-		"DB_PASSWD" => "root"
+		"DB_PASSWD" => "devel3757"
 );
 
 $DefaultISOCountry = "SG";
