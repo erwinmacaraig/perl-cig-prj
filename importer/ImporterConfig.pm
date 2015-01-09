b5973ad3e6f52bb04ed117a0b911367d47d6b68e
iskalot <jervy.escoto@gmail.com>
2015-01-09 20:32:37 +0800

    IMPORTER - age levels

--- importer/ImporterConfig.pm
+++ importer/ImporterConfig.pm
@@ -2,9 +2,9 @@ package ImporterConfig;
 require Exporter;
 @ISA =  qw(Exporter);
 %DB_CONFIG = (
-		"DB_DSN" => "DBI:mysql:fifaSingaporeTest", #fifasponline
+		"DB_DSN" => "DBI:mysql:fifasponline_demo", #fifasponline
 		"DB_USER" => "root",
-		"DB_PASSWD" => "devel3757"
+		"DB_PASSWD" => "root"
 );
 
 $DefaultISOCountry = "SG";
@@ -12,4 +12,18 @@ $DefaultISOCountry = "SG";
 @PREDEFINED = (
     "NOW()"
 );
+
+#%MADefaultstrImportEntityCodeID = 1;
+
+$AGE_BREAKPOINT_PLAYER_PROFESSIONAL = "AGE_BREAKPOINT_PLAYER_PROFESSIONAL";
+$AGE_BREAKPOINT_PLAYER_AMATEUR_U_C = 16;
+
+%ADULT_AGE = (
+    "FROM" => 21,
+    "TO" => 100,
+);
+
+$AGE_LEVEL_ADULT = "ADULT";
+$AGE_LEVEL_MINOR = "MINOR";
+
 1;
