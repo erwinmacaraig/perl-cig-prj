29b28f9a62b0ee707fcc002aa7a12f09edc53f4a
Erwin Macaraig <erwin.macaraig@gmail.com>
2015-01-23 14:40:35 +0800

    test values for FC-639

---
+++ db_setup/test_data/150123_tblWFRuleDocuments.sql
@@ -0,0 +1,40 @@
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '53', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '54', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '55', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '56', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '57', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '58', '1', '1', '1', '0'); 
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '59', '1', '1', '1', '0'); 
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '60', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1470', '61', '1', '1', '1', '0');
+
+
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '53', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '54', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '55', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '56', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '57', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '58', '1', '1', '1', '0'); 
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '59', '1', '1', '1', '0'); 
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '60', '1', '1', '1', '0');
+
+INSERT INTO tblWFRuleDocuments (intWFRuleID, intDocumentTypeID, intAllowApprovalEntityAdd, intAllowApprovalEntityVerify, intAllowProblemResolutionEntityAdd, intAllowProblemResolutionEntityVerify) VALUES ('1471', '61', '1', '1', '1', '0');
+
+
+
