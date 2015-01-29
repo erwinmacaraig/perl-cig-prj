b259bb6d4df60cba167c651c64ae73ff7535b26c
Erwin Macaraig <erwin.macaraig@gmail.com>
2015-01-29 09:39:28 +0800

    Adding SQL testdata and rules for Adding Venue Documents

---
+++ db_setup/test_data/150129_tblWFRule.sql
@@ -0,0 +1,4 @@
+INSERT INTO tblWFRule (intWFRuleID,intRealmID, intSubRealmID, intOriginLevel, strWFRuleFor, intEntityLevel, strRegistrationNature,intApprovalEntityLevel, intProblemResolutionEntityLevel, strTaskType, strTaskStatus, intDocumentTypeID) 
+VALUES 
+(1472,1,0,3,'ENTITY',-47,'AMENDMENT',100,3,'APPROVAL','ACTIVE',0), 
+(1473,1,0,100,'ENTITY',-47,'AMENDMENT',100,100,'APPROVAL','ACTIVE',0);
