7a827b9a8838e155ed8235683e7677364da192b2
Bruce Irvine <bruce.g.irvine@gmail.com>
2015-02-12 10:43:10 +1100

    Debug code

--- web/WorkFlow.pm
+++ web/WorkFlow.pm
@@ -1439,7 +1439,7 @@ sub checkForOutstandingTasks {
                     updateSphinx($db,$Data->{'cache'}, 'Person','update',$personObject);
                 }
                 # if check  pass call save
-                if($ppref->{'strPersonType'} eq 'PLAYER' and $Data->{'SystemConfig'}{'cleanPlayerPersonRecords'})   {
+                if($ppref->{'strPersonType'} eq 'PLAYER' and $Data->{'SystemConfig'}{'cleanPlayerPersonRecords'}) {
                     PersonRegistration::cleanPlayerPersonRegistrations($Data, $personID, $personRegistrationID);
                 }
     
