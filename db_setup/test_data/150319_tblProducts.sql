83b6fb764de33fe6312fc12e5487142acf90c901
iskalot <jervy.escoto@gmail.com>
2015-03-19 11:59:21 +0800

    FC-759 - products Coach and Referee

---
+++ db_setup/test_data/150319_tblProducts.sql
@@ -0,0 +1,4 @@
+UPDATE tblProducts SET intProductNationalPeriodID = 8, strDisplayName = 'V50 Referee insurance' WHERE intProductID = 84;
+UPDATE tblProducts SET intProductNationalPeriodID = 17, strDisplayName = 'V50 Referee insurance' WHERE intProductID = 85;
+UPDATE tblProducts SET intProductNationalPeriodID = 8, strDisplayName = 'UEFA coach card payment, 3 years' WHERE intProductID = 83;
+
---
+++ db_setup/test_data/150319_tblRegistrationItem.sql
@@ -0,0 +1,21 @@
+delete from tblRegistrationItem where strPersonType = 'REFEREE' and strItemType = 'PRODUCT';
+delete from tblRegistrationItem where strPersonType = 'COACH' and strItemType = 'PRODUCT';
+
+
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',100,'NEW','REFEREE','','FOOTBALL','','PRODUCT',84,1,0,'2015-03-01 18:45:09',0,'',NULL,NULL,0,70);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',20,'NEW','REFEREE','','FOOTBALL','','PRODUCT',84,1,0,'2015-03-04 06:10:54',0,'',NULL,NULL,0,70);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,20,'REGO','',20,'NEW','REFEREE','','FOOTBALL','','PRODUCT',84,1,0,'2015-03-04 06:11:06',0,'',NULL,NULL,0,70);
+
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',100,'NEW','REFEREE','','FUTSAL','','PRODUCT',85,1,0,'2015-03-01 18:45:09',0,'',NULL,NULL,0,70);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',20,'NEW','REFEREE','','FUTSAL','','PRODUCT',85,1,0,'2015-03-04 06:10:54',0,'',NULL,NULL,0,70);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,20,'REGO','',20,'NEW','REFEREE','','FUTSAL','','PRODUCT',85,1,0,'2015-03-04 06:11:06',0,'',NULL,NULL,0,70);
+
+
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,3,'REGO','',3,'NEW','COACH','','FOOTBALL','','PRODUCT',83,1,1,'2015-03-04 03:55:57',1,'',NULL,NULL,0,0);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,20,'REGO','',20,'NEW','COACH','','FOOTBALL','','PRODUCT',83,1,1,'2015-03-04 03:55:57',1,'',NULL,NULL,0,0);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',3,'NEW','COACH','','FOOTBALL','','PRODUCT',83,1,1,'2015-03-04 03:55:57',1,'',NULL,NULL,0,0);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,100,'REGO','',20,'NEW','COACH','','FOOTBALL','','PRODUCT',83,1,1,'2015-03-04 03:55:57',1,'',NULL,NULL,0,0);
+INSERT INTO `tblRegistrationItem` VALUES (0,1,0,20,'REGO','',3,'NEW','COACH','','FOOTBALL','','PRODUCT',83,1,1,'2015-03-04 03:55:57',1,'',NULL,NULL,0,0);
+
+
+
