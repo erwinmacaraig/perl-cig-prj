2802a6cfe2b82df6fc15373e96d076797436e579
warren <warren@rodie.net>
2015-03-27 15:17:54 +1100

    Fix bad script path

--- web/js/fc-common.js
+++ web/js/fc-common.js
@@ -155,7 +155,7 @@ function updateRegoProductsTotal(chkb,id_cost,id_total,client){
 	$.ajax(
 		{
 			method: "POST",
-			url:"../formatcurrencyamount.cgi",
+			url:"formatcurrencyamount.cgi",
 			data:"amount=" + total + "&client="+ client 			
 		}).done(
 			function(formattedamount){
