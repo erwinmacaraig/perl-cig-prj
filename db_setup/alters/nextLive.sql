1a5d6f19ace6150b4b7b89863402098f5f938d4e
baff <bruce.g.irvine@gmail.com>
2015-04-22 10:33:49 +1000

    FC-962 and FC877 DB Alters in nextLive.sql

--- db_setup/alters/nextLive.sql
+++ db_setup/alters/nextLive.sql
@@ -76,3 +76,26 @@ ALTER TABLE tblPersonRegistration_1
 ALTER TABLE tblWFRule
     ADD COLUMN intUsingPersonLevelChangeFilter tinyint default 0 COMMENT 'Using Person Level change filter',
     ADD COLUMN intPersonLevelChange tinyint default 0 COMMENT 'Was Person Level changed';
+
+
+# 150421_tblPersonRegistration.sql - FC877
+ALTER TABLE tblPersonRegistration_1
+ADD COLUMN `intOnLoan` INT NULL DEFAULT 0 AFTER `strPreviousPersonLevel`;
+
+# 150421_tblPersonRequest.sql - FC877
+ALTER TABLE tblPersonRequest
+ADD COLUMN `dtLoanFrom` DATETIME NULL AFTER `strRequestStatus`,
+ADD COLUMN `dtLoanTo` DATETIME NULL AFTER `dtLoanFrom`,
+ADD COLUMN `intOpenLoan` INT NULL DEFAULT 0 AFTER `dtLoanTo`,
+ADD COLUMN `strTMSReference` VARCHAR(100) NULL AFTER `intOpenLoan`;
+
+# 150421_tblPerson.sql - FC877
+ALTER TABLE tblPerson
+ADD COLUMN `strInternationalTransferSourceClub` VARCHAR(150) NULL DEFAULT NULL AFTER `strGuardianRelationship`,
+ADD COLUMN `dtInternationalTransferDate` DATETIME NULL AFTER `strInternationalTransferSourceClub`,
+ADD COLUMN `strInternationalTransferTMSRef` VARCHAR(100) NULL DEFAULT NULL AFTER `dtInternationalTransferDate`,
+ADD COLUMN `strInternationalLoanSourceClub` VARCHAR(150) NULL DEFAULT NULL AFTER `strInternationalTransferTMSRef`,
+ADD COLUMN `strInternationalLoanTMSRef` VARCHAR(100) NULL DEFAULT NULL AFTER `strInternationalLoanSourceClub`,
+ADD COLUMN `dtInternationalLoanFromDate` DATETIME NULL AFTER `strInternationalLoanTMSRef`,
+ADD COLUMN `dtInternationalLoanToDate` DATETIME NULL AFTER `dtInternationalLoanFromDate`,
+ADD COLUMN `intInternationalLoan` INT NULL DEFAULT 0 AFTER `dtInternationalLoanToDate`;
