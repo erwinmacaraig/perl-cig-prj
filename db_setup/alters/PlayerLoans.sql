8fc231100dbd2de7cb8d3e64a74f065f93bcd5f4
iskalot <jervy.escoto@gmail.com>
2015-04-23 17:36:19 +0800

    FC-887 - changed DATETIME to DATE

--- db_setup/alters/PlayerLoans.sql
+++ db_setup/alters/PlayerLoans.sql
@@ -1,18 +1,18 @@
 ALTER TABLE tblPerson
 ADD COLUMN `strInternationalTransferSourceClub` VARCHAR(150) NULL DEFAULT NULL AFTER `strGuardianRelationship`,
-ADD COLUMN `dtInternationalTransferDate` DATETIME NULL AFTER `strInternationalTransferSourceClub`,
+ADD COLUMN `dtInternationalTransferDate` DATE NULL AFTER `strInternationalTransferSourceClub`,
 ADD COLUMN `strInternationalTransferTMSRef` VARCHAR(100) NULL DEFAULT NULL AFTER `dtInternationalTransferDate`,
 ADD COLUMN `strInternationalLoanSourceClub` VARCHAR(150) NULL DEFAULT NULL AFTER `strInternationalTransferTMSRef`,
 ADD COLUMN `strInternationalLoanTMSRef` VARCHAR(100) NULL DEFAULT NULL AFTER `strInternationalLoanSourceClub`,
-ADD COLUMN `dtInternationalLoanFromDate` DATETIME NULL AFTER `strInternationalLoanTMSRef`,
-ADD COLUMN `dtInternationalLoanToDate` DATETIME NULL AFTER `dtInternationalLoanFromDate`,
+ADD COLUMN `dtInternationalLoanFromDate` DATE NULL AFTER `strInternationalLoanTMSRef`,
+ADD COLUMN `dtInternationalLoanToDate` DATE NULL AFTER `dtInternationalLoanFromDate`,
 ADD COLUMN `intInternationalLoan` INT NULL DEFAULT 0 AFTER `dtInternationalLoanToDate`;
 
 ALTER TABLE tblPersonRegistration_1
 ADD COLUMN `intOnLoan` INT NULL DEFAULT 0 AFTER `strPreviousPersonLevel`;
 
 ALTER TABLE tblPersonRequest
-ADD COLUMN `dtLoanFrom` DATETIME NULL AFTER `strRequestStatus`,
-ADD COLUMN `dtLoanTo` DATETIME NULL AFTER `dtLoanFrom`,
+ADD COLUMN `dtLoanFrom` DATE NULL AFTER `strRequestStatus`,
+ADD COLUMN `dtLoanTo` DATE NULL AFTER `dtLoanFrom`,
 ADD COLUMN `intOpenLoan` INT NULL DEFAULT 0 AFTER `dtLoanTo`,
 ADD COLUMN `strTMSReference` VARCHAR(100) NULL AFTER `intOpenLoan`;
