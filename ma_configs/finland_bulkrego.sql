74c4b23fb317cb440aaaf4bb108cad3a49986574
baff <bruce.g.irvine@gmail.com>
2015-04-30 09:34:53 +1000

    SQL for setup

---
+++ ma_configs/finland_bulkrego.sql
@@ -0,0 +1,43 @@
+##MAOFFICIAL
+#100
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='MAOFFICIAL', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='MAOFFICIAL', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='MAOFFICIAL', strPersonLevel='', strSport='FUTSAL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='MAOFFICIAL', strPersonLevel='', strSport='FUTSAL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+
+##CLUBOFFICIAL
+#100
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+#20
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+#3
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=3, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=3, intEntityLevel=3, strPersonType='CLUBOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+
+##TEAMOFFICIAL
+#100
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=3, strPersonType='TEAMOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=3, strPersonType='TEAMOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+#20
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=3, strPersonType='TEAMOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=3, strPersonType='TEAMOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+#3
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=3, intEntityLevel=3, strPersonType='TEAMOFFICIAL', strPersonLevel='', strSport='', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+
+##REFEREE
+#100
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=100, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=100, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+#20
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FOOTBALL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='ADULT', intPaymentRequired=1, intOfEntityLevel=1;
+INSERT INTO tblMatrix SET intRealmID=1, strRegistrationNature='RENEWAL', strWFRuleFor='BULKREGO', intOriginLevel=20, intEntityLevel=20, strPersonType='REFEREE', strPersonLevel='', strSport='FUTSAL', strAgeLevel='MINOR', intPaymentRequired=1, intOfEntityLevel=1;
