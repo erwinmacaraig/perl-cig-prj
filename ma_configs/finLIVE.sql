2a65fff76913905774df936a22a82755e8eee0ba
baff <bruce.g.irvine@gmail.com>
2015-05-25 12:42:00 +1000

    Live script

---
+++ ma_configs/finLIVE.sql
@@ -0,0 +1,11 @@
+
+UPDATE tblPaymentConfig SET strGatewayUsername="545178", strGatewayPassword="QbvzGnDAD2qFyywo3SSlEp9D8OiXNsEZK5Mb8EqGCJRi9KMZvZNXbaZPlUjSG2S9rnOcRaY16QtoSdQh" WHERE intPaymentConfigID=3;
+UPDATE tblSystemConfig SET strValue=1 WHERE intSystemConfigID=3955;
+UPDATE tblProducts SET curDefaultAmount=3 WHERE intProductID=59;
+
+## ONCE DONE
+
+UPDATE tblPaymentConfig SET strGatewayUsername="375917", strGatewayPassword="SAIPPUAKAUPPIAS" WHERE intPaymentConfigID=3;
+UPDATE tblSystemConfig SET strValue=0 WHERE intSystemConfigID=3955;
+UPDATE tblProducts SET curDefaultAmount=45 WHERE intProductID=59;
+
