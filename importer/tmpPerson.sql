adb40de55f552203ebb19897bb4e8c2e9373c234
baff <bruce.g.irvine@gmail.com>
2015-05-29 18:13:52 +1000

    Work on new script for Person tmp

---
+++ importer/tmpPerson.sql
@@ -0,0 +1,40 @@
+#PEOPLE FILE
+#SystemID;PalloID;Status;LocalFirstName;LocalLastName;LocalPreviousLastName;LocalLanguageCode;PreferedName;LatinFirstName;LatinLastName;LatinPreviousLastName;DateOfBirth;Gender;Nationality;CountryOfBirth;RegionOfBirth;PlaceOfBirth;Fax;Phone;Address1;Address2;PostalCode;Town;Suburb;Email;Identifier;IdentifierType;CountryIssued;DateFrom;DateTo
+
+
+DROP TABLE IF EXISTS tmpPerson;
+CREATE TABLE tmpPerson(
+    intID INT NOT NULL AUTO_INCREMENT,
+    strFileType varchar(30) default '',
+    strPersonCode varchar(30) default '',
+    strNationalNum varchar(30) default '',
+    strStatus varchar(30) default '',
+    strLocalFirstname varchar(100) default '',
+    strLocalSurname varchar(100) default '',
+    strLocalMaidenName varchar(100) default '',
+    strLatinFirstname varchar(100) default '',
+    strISONationality varchar(100) default '',
+    strISOCountryOfBirth varchar(100) default '',
+    strRegionOfBirth varchar(100) default '',
+    strPlaceOfBirth varchar(100) default '',
+    strFax varchar(100) default '',
+    strPhone varchar(100) default '',
+    strAddress1 varchar(100) default '',
+    strAddress2 varchar(100) default '',
+    strPostalCode varchar(15) default '',
+    strSuburb varchar(100) default '',
+    strEmail varchar(200) default '',
+    strLocalLanguage varchar(100) default '',
+    strGender varchar(10) default '',
+    dtDOB date,
+
+    strIdentifier varchar(100) default '',
+    strIdentifierType varchar(100) default '',
+    strIdentifierCountryIssued varchar(100) default '',
+    dtIdentifierFrom date,
+    dtIdentifierTo date,
+    
+    PRIMARY KEY (intID),
+    INDEX index_strFileType (strFileType),
+    INDEX index_personCode (strPersonCode)
+) DEFAULT CHARACTER SET = utf8;
