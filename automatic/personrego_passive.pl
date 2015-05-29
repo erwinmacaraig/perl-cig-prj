45628a090aa491fb54fe7e1a4f163183b77c9783
baff <bruce.g.irvine@gmail.com>
2015-05-29 10:44:54 +1000

    Work on FC-256 script to end regos

--- automatic/personrego_passive.pl
+++ automatic/personrego_passive.pl
@@ -8,61 +8,36 @@ use Defs;
 use Data::Dumper;
 
 {
-    my %Data = ();
     my $db = connectDB();
-    my @realm;
+    my $realmID = 1;
 
-    $Data{'db'} = $db;
-
-    my $targetRealm = getRealm(\%Data);
-    my $st = undef;
-    my $q;
-
-    while(my $realm = $targetRealm->fetchrow_hashref()) {
-
-        $st = qq [
-            UPDATE
-                tblPersonRegistration_$realm->{'intRealmID'} as PR
-		INNER JOIN tblNationalPeriod as NP ON (
-			NP.intNationalPeriodID = PR.intNationalPeriodID
-		)
-            SET
-                PR.strStatus = ?
-            WHERE
-                (
-			(
-				NP.dtTo > '1900-01-01'
-				AND NP.dtTo < DATE(NOW())
-			)
-			OR
-			(
-				PR.dtTo > '1900-01-01'
-				AND PR.dtTo < DATE(NOW())
-			)
-		)
-                AND PR.strStatus = 'ACTIVE'
-        ];
-
-        $q = $db->prepare($st);
-        $q->execute(
-            $Defs::PERSONREGO_STATUS_PASSIVE
-        ) or query_error($st);
-    }
-}
-
-sub getRealm {
-    my ($Data) = @_;
-
-    my $st = qq[
-        SELECT 
-            intRealmID
-        FROM 
-            tblRealms
+    my $st = qq [
+        UPDATE
+            tblPersonRegistration_$realmID as PR
+    		INNER JOIN tblNationalPeriod as NP ON (
+    			NP.intNationalPeriodID = PR.intNationalPeriodID
+	    	)
+        SET
+            PR.strStatus = ?
+        WHERE
+            (
+			    (
+				    NP.dtTo > '1900-01-01'
+				    AND NP.dtTo < DATE(NOW())
+			    )
+			    OR
+			    (
+				    PR.dtTo > '1900-01-01'
+				    AND PR.dtTo < DATE(NOW())
+                    AND intOnLoan=0
+			    )
+		    )
+            AND PR.strStatus = 'ACTIVE'
     ];
 
-    my $db = $Data->{'db'};
     my $q = $db->prepare($st);
-    $q->execute() or query_error($st);
-
-    return $q;
+    $q->execute(
+        $Defs::PERSONREGO_STATUS_PASSIVE
+    ) or query_error($st);
 }
+1;
