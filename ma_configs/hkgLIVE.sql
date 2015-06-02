2cbb07ad5a38e1af2c7499ec858a2c21ab70112a
baff <bruce.g.irvine@gmail.com>
2015-06-02 11:58:14 +1000

    importer scripts

--- ma_configs/hkgLIVE.sql
+++ ma_configs/hkgLIVE.sql
@@ -1,6 +1,5 @@
 ## FOR LIVE
-UPDATE tblPaymentConfig SET strGatewayUsername="545178", strGatewayPassword="QbvzGnDAD2qFyywo3SSlEp9D8OiXNsEZK5Mb8EqGCJRi9KMZvZNXbaZPlUjSG2S9rnOcRaY16QtoSdQh" WHERE intPaymentConfigID=3;
-UPDATE tblSystemConfig SET strValue=1 WHERE intSystemConfigID=3955;
+#UPDATE tblPaymentConfig SET strGatewayUsername="545178", strGatewayPassword="QbvzGnDAD2qFyywo3SSlEp9D8OiXNsEZK5Mb8EqGCJRi9KMZvZNXbaZPlUjSG2S9rnOcRaY16QtoSdQh" WHERE intPaymentConfigID=3;
 SELECT * FROM tblSystemConfig WHERE strOption  LIKE 'lockAp%'; ##
 UPDATE tblEntity SET strImportEntityCode='1248' WHERE intEntityLevel=100; 
 UPDATE tblSystemConfig SET strValue=0 WHERE strOption  LIKE '%Venue%';
@@ -8,7 +7,7 @@ UPDATE tblSystemConfig SET strValue=0 WHERE strOption  LIKE '%Venue%';
 ## CHECK THIS
 INSERT INTO tblSystemConfig VALUES (0, 1, 'selfRego_RENEW_PLAYER', 1, NOW(),1,0);
 INSERT INTO tblSystemConfig VALUES (0,1, 'allowFindPaymentMinLevel', 100, NOW(),1,0);
-INSERT INTO tblSystemConfig VALUES (0,1, 'paymentPrefix', 'FAF15-', NOW(),1,0); ###TEST
+INSERT INTO tblSystemConfig VALUES (0,1, 'paymentPrefix', 'HKG15-', NOW(),1,0); ###TEST
 UPDATE tblGenerate SET intCurrentNum = (SELECT MAX(strNationalNum) + 1 FROM tblPerson) WHERE strGenType='PERSON';
 
 
@@ -23,5 +22,5 @@ UPDATE tblSystemConfig SET strValue=0 WHERE strOption  LIKE '%Venue%';
 
 ## Update below per import run
 DELETE FROM tblSystemConfig WHERE strOption = 'paymentPrefix';
-INSERT INTO tblSystemConfig VALUES (0,1,'paymentPrefix', 'FAFUAT2-', NOW(),1,0);
-INSERT INTO tblSystemConfig VALUES (0,1,'paymentPrefix', 'FAFTRN2-', NOW(),1,0);
+INSERT INTO tblSystemConfig VALUES (0,1,'paymentPrefix', 'HGKUAT2-', NOW(),1,0);
+INSERT INTO tblSystemConfig VALUES (0,1,'paymentPrefix', 'HGKTRN2-', NOW(),1,0);
