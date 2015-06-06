24b9ecf87464167fa5685187b48b5b184e984b70
baff <bruce.g.irvine@gmail.com>
2015-06-06 19:31:28 +1000

    More work

--- importer/brucetmpRegoFix.sql
+++ importer/brucetmpRegoFix.sql
@@ -37,13 +37,18 @@ then
 
 INSERT IGNORE tmpRegoFix SELECT intPersonRegistrationID , intPersonID, intEntityID, strStatus, strPersonType, strPersonLevel, strSport, intNationalPeriodID FROM tblPersonRegistration_1 WHERE strStatus IN ("PASSIVE", "ACTIVE") and intNationalPeriodID <=120 AND strPersonType IN ("COACH", "PLAYER", "REFEREE") ORDER BY dtFrom DESC, strStatus, tmpisPaid DESC;
 
-UPDATE tblPersonRegistration_1 SET strStatus='ROLLED_OVER' WHERE strStatus='PASSIVE' AND strPersonType IN ("COACH", "PLAYER", "REFEREE") and intNationalPeriodID <=120 AND intOnLoan = 0 and intIsLoanedOut = 0;
+
+UPDATE tblPersonRegistration_1 SET strStatus='ROLLED_OVER' WHERE strStatus='PASSIVE' AND strPersonType IN ("PLAYER") and intNationalPeriodID <=120 AND intOnLoan = 0 and intIsLoanedOut = 0;
 UPDATE tblPersonRegistration_1 SET strStatus='ROLLED_OVER' WHERE strStatus='PASSIVE' AND strPersonType IN ("REFEREE") and intNationalPeriodID <=120 AND intOnLoan = 0 and intIsLoanedOut = 0;
 
 UPDATE tblPersonRegistration_1 as PR INNER JOIN tmpRegoFix as T ON (PR.intPersonRegistrationID = T.intID) SET PR.strStatus = T.strStatus WHERE and intOnLoan = 0 and intIsLoanedOut = 0;
+
 UPDATE tblPersonRegistration_1 as PR INNER JOIN tmpRegoFix as T ON (PR.intPersonRegistrationID = T.intID) SET PR.strStatus = T.strStatus WHERE and intOnLoan = 0 and intIsLoanedOut = 0 AND PR.strPersonType='REFEREE';
 
 
 SELECT strStatus, intPersonRegistrationID FROM tblPersonRegistration_1 WHERE strPersonType IN ('PLAYER', 'COACH', 'REFEREE') AND strStatus NOT IN ('TRANSFERRED') LIMIT 10;
 Then build up a 
 UPDATE tblPersonRegistration_1 SET strStatus=X WHERE intPersonRegistrationID=X;
+
+UPDATE tblPersonRegistration_1 SET strStatus='ROLLED_OVER' WHERE strStatus='PASSIVE' AND strPersonType IN ("PLAYER") and intNationalPeriodID <=120 AND intOnLoan = 0 and intIsLoanedOut = 0;
+UPDATE tblPersonRegistration_1 as PR INNER JOIN tmpRegoFix as T ON (PR.intPersonRegistrationID = T.intID) SET PR.strStatus = T.strStatus WHERE and intOnLoan = 0 and intIsLoanedOut = 0;
