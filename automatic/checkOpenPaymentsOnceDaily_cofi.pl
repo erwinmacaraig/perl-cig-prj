b167421dbec6b8a9945055943004387f4ac0507a
baff <bruce.g.irvine@gmail.com>
2015-08-14 09:34:23 +1000

    Work on handling "error"

--- automatic/checkOpenPaymentsOnceDaily_cofi.pl
+++ automatic/checkOpenPaymentsOnceDaily_cofi.pl
@@ -140,13 +140,19 @@ print STDERR "IN checkOpenPayments\n";
         my $retval = $res->content() || '';
 print STDERR Dumper(\%APIResponse);
 print STDERR "--- $retval\n";
-	next if $retval =~/error/;
-	next if $retval !~/status/;
+	#next if $retval =~/error/;
+	#next if $retval !~/status/; 
+	#my $dataIN= XMLin($retval);
 
-
-        #my $dataIN= XMLin($retval);
-	my $dataIN= XMLin($retval);
-
+    my $dataIN= {};
+    if ($retval =~/error/)  {
+        $dataIN->{'status'} = 1;
+print STDERR "IN HERE for $logID\n";
+    }
+    else    {
+	    next if $retval !~/status/; 
+        $dataIN= XMLin($retval);
+    }
         #print STDERR Dumper($dataIN);
         
         $APIResponse{'STATUS'} = $dataIN->{'status'}; 
