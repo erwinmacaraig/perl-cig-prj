7cd0ff5a40fdf1a2e112978cba83b16e408a0587
baff <bruce.g.irvine@gmail.com>
2015-10-15 04:26:37 +1100

    Work on 2016 Custom FIN files

---
+++ SQLreports/FIN/FC1410_Data_for_Competition_System_2016.sql
@@ -0,0 +1,52 @@
+SELECT 
+    UCASE(strISONationality) AS strISONationality, 
+	CASE PR.strSport
+                WHEN 'FOOTBALL' THEN 'Football'
+                WHEN 'FUTSAL' THEN 'Futsal'
+                ELSE ''
+            END as Sport,
+
+    PR.dtTo AS PRdtTo, 
+    strLocalSurname, 
+    tblRegion.strLocalName AS strRegionName, 
+    tblPerson.dtDOB AS dtDOB, 
+  IF(intGender=1, 'Male', IF(intGender=2, 'Female', '')) as gender,
+	CASE PR.strPersonLevel
+                WHEN 'PROFESSIONAL' THEN 'Professional'
+                WHEN 'AMATEUR' THEN 'Amateur'
+                WHEN 'HOBBY' THEN 'Hobby'
+                WHEN 'AMATEUR_U_C' THEN 'Amateur (Under Contract)'
+                ELSE ''
+            END as PersonLevel,
+    PR.dtFrom AS PRdtFrom, 
+    strLocalFirstname, 
+    tblClub.strLocalName AS strClubName, 
+    strNationalNum
+        FROM
+
+            tblPerson
+            INNER JOIN tblPersonRegistration_1 as PR ON (
+                tblPerson.intPersonID=PR.intPersonID
+            )
+            INNER JOIN tblEntity as E ON (
+                E.intEntityID = PR.intEntityID
+            )
+            LEFT JOIN tblEntity as tblClub ON (
+                PR.intEntityID = tblClub.intEntityID
+                AND tblClub.intEntityLevel = 3
+            )
+            LEFT JOIN tblTempEntityStructure as RL ON (
+                E.intEntityID = RL.intChildID
+                AND RL.intParentLevel = 20
+            )
+            LEFT JOIN tblEntity as tblRegion ON (
+                RL.intParentID = tblRegion.intEntityID
+            )
+
+
+
+
+            LEFT JOIN tblTempEntityStructure as TempEnt ON (TempEnt.intChildID=PR.intEntityID)
+        WHERE
+             PR.strPersonType = 'PLAYER'  AND  PR.strStatus = 'ACTIVE'  AND  PR.intNationalPeriodID = '121'
+            AND TempEnt.intParentID = 1
