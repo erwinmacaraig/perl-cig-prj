5e84de402f9df16a0bc1649a0fdbdfbe3481f391
baff <bruce.g.irvine@gmail.com>
2016-03-07 09:37:22 +1100

    FC1916 SQL fix

--- web/ListAuditLog.pm
+++ web/ListAuditLog.pm
@@ -30,115 +30,77 @@ use TTTemplate;
 sub listPersonAuditLog    {
 	my ($Data, $personID) = @_;
 
-	my $query = qq[ 
-        SELECT
-            AL.strUsername,
-            AL.strType,
-            AL.strSection,
-            AL.strLocalName,
-            AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-            LEFT JOIN tblPersonRegistration_$Data->{'Realm'} as PR ON (
-                PR.intPersonRegistrationID=AL.intID 
-                AND AL.strSection="Person Registration"
-            )
-            LEFT JOIN tblWFTask as WFT ON (
-                WFT.intWFTaskID = AL.intID 
-                AND AL.strSection="WFTask"
-            )
-            LEFT JOIN tblPerson as P ON (
-                P.intPersonID= AL.intID 
-                AND AL.strSection IN ("Player Passport", "PERSON", "Person")
-            )
-        WHERE
-            (
-                AL.intID=? 
-                OR PR.intPersonID=? 
-                OR WFT.intPersonID=? 
-                OR P.intPersonID=?
-            )
-            AND AL.strSection NOT IN ('Person Entity')
-            AND (
-                AL.strSection IN ("Player Passport", "PERSON", "Person", "Person Registration")
-                OR (AL.strSection = "WFTask" AND WFT.intPersonID=?)
-            )
-        ORDER BY 
-            AL.dtUpdated DESC
-    
-	];
-    $query = qq[
+    my $query = qq[
         (
-        SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-        WHERE
-            AL.intEntityID=18
-            AND AL.strSection IN ("Person Registration")
-            AND AL.strType  LIKE 'Update Person Registration%'
-            AND AL.intEntityTypeID=-1
-    )
-UNION ALL
-    (
-        SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-        WHERE
-            AL.intEntityID=?
-            AND AL.strSection IN ("Person Registration")
-            AND AL.strType  LIKE 'Update Person Registration%'
-            AND AL.intEntityTypeID>=0
-    )
-UNION ALL
-     (
-        SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-            INNER JOIN tblPersonRegistration_1 as PR ON (
-                PR.intPersonRegistrationID=AL.intID
-                AND AL.strSection="Person Registration"
-            )
-        WHERE
-            PR.intPersonID=?
-            AND AL.strSection NOT IN ('Person Entity')
-            AND AL.strSection IN ("Player Passport", "PERSON", "Person", "Person Registration")
-            AND AL.strType NOT LIKE 'Update Person Registration%'
-    )
-UNION ALL
-    (
-     SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-            LEFT JOIN tblWFTask as WFT ON (
-                WFT.intWFTaskID = AL.intID
-                AND AL.strSection="WFTask"
-            )
-        WHERE
-            WFT.intPersonID=?
-            AND AL.strSection NOT IN ('Person Entity')
-            AND (AL.strSection = "WFTask" AND WFT.intPersonID=?)
-    )
-UNION ALL
-    (
-     SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
-        FROM
-            tblAuditLog as AL
-            INNER JOIN tblPerson as P ON (
-                P.intPersonID= AL.intID
-                AND AL.strSection IN ("Player Passport", "PERSON", "Person")
-            )
-        WHERE
-            (
-                AL.intID=?
-                OR P.intPersonID=?
-            )
-            AND AL.strSection NOT IN ('Person Entity')
-            AND (
-                AL.strSection IN ("Player Passport", "PERSON", "Person")
-            )
-    )
-    ORDER BY
+            SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
+            FROM
+                tblAuditLog as AL
+            WHERE
+                AL.intEntityID=?
+                AND AL.strSection IN ("Person Registration")
+                AND AL.strType  LIKE 'Update Person Registration%'
+                AND AL.intEntityTypeID>=0
+        )
+        UNION ALL
+        (
+            SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
+            FROM
+                tblAuditLog as AL
+                INNER JOIN tblPersonRegistration_1 as PR ON (
+                    PR.intPersonRegistrationID=AL.intID
+                    AND AL.strSection="Person Registration"
+                )
+            WHERE
+                PR.intPersonID=?
+                AND AL.strSection NOT IN ('Person Entity')
+                AND AL.strSection IN ("Player Passport", "PERSON", "Person", "Person Registration")
+                AND (
+                    (
+                        AL.strType NOT LIKE 'Update Person Registration%'
+                    )
+                    OR
+                    (
+                        AL.strType LIKE 'Update Person Registration%'
+                        AND AL.intEntityTypeID=-1
+                    )
+                )
+        )
+        UNION ALL
+        (
+             SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
+                FROM
+                    tblAuditLog as AL
+                    LEFT JOIN tblWFTask as WFT ON (
+                        WFT.intWFTaskID = AL.intID
+                        AND AL.strSection="WFTask"
+                    )
+                WHERE
+                    WFT.intPersonID=?
+                    AND AL.strSection NOT IN ('Person Entity')
+                    AND (AL.strSection = "WFTask" AND WFT.intPersonID=?)
+        )
+        UNION ALL
+        (
+             SELECT DISTINCT AL.strUsername, AL.strType, AL.strSection, AL.strLocalName, AL.dtUpdated
+                FROM
+                    tblAuditLog as AL
+                    INNER JOIN tblPerson as P ON (
+                        P.intPersonID= AL.intID
+                        AND AL.strSection IN ("Player Passport", "PERSON", "Person")
+                    )
+                WHERE
+                    (
+                        AL.intID=?
+                        OR P.intPersonID=?
+                    )
+                    AND AL.strSection NOT IN ('Person Entity')
+                    AND (
+                        AL.strSection IN ("Player Passport", "PERSON", "Person")
+                    )
+        )
+        ORDER BY
             dtUpdated DESC
+
     ];
 	my $sth = $Data->{'db'}->prepare($query);
 	$sth->execute(
