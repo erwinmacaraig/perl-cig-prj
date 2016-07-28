132594ffc9b17b852a445749a214585bf5cf7c2b
baff <bruce.g.irvine@gmail.com>
2016-07-28 18:24:59 +1000

    Minor SQL fix

--- web/Flow_IntTransferReturnBackend.pm
+++ web/Flow_IntTransferReturnBackend.pm
@@ -1616,7 +1616,7 @@ sub deleteExistingReg {
                 ON TL.intLogID = TX.intTransLogID
         WHERE 
             TX.intPersonRegistrationID = ?
-            AND (TL.intStatus = 0 OR (TL.intStatus= 1 and TL.intAmount>0))
+            AND (TL.intStatus = 0 OR (TL.intStatus= 1 and TL.intAmount=0))
             AND TL.intRealmID = ?
             AND TL.intSentToGateway = 0
             AND TX.intSentToGateway = 0
